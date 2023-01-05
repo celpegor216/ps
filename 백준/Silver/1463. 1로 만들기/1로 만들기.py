55345f97331d0c4929e5e6cfdb38cc38c157a952
조예지 <97578535+celpegor216@users.noreply.github.com>
2023-01-05 21:21:27 +0900

    [Silver III] Title: 1로 만들기, Time: 400 ms, Memory: 218692 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -12,26 +12,23 @@ N = int(input())
 dp = [[0, 0] for _ in range(4)]
 
 for i in range(2, N + 1):
+    min_lst = []
+
     if not i % 3:
         dp[0].append(i // 3)
+        min_lst.append(dp[3][i // 3])
     else:
         dp[0].append(-1)
 
     if not i % 2:
         dp[1].append(i // 2)
+        min_lst.append(dp[3][i // 2])
     else:
         dp[1].append(-1)
 
     dp[2].append(i - 1)
+    min_lst.append(dp[3][i - 1])
 
-    div_3, div_2, min_1 = dp[3][dp[0][i]], dp[3][dp[1][i]], dp[3][dp[2][i]]
-    min_lst = []
-    if div_3 > -1:
-        min_lst.append(div_3)
-    if div_2 > -1:
-        min_lst.append(div_2)
-    if min_1 > -1:
-        min_lst.append(min_1)
     dp[3].append(min(min_lst) + 1)
 
 print(dp[3][N])
