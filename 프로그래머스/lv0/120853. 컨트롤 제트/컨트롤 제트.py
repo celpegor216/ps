9ceb5378f257b39c15bc9fb1a1e7fcfdee10fc71
조예지 <97578535+celpegor216@users.noreply.github.com>
2023-01-11 21:14:53 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = 0
+    
+    lst = s.split()
+    
+    for i in range(len(lst)):
+        if lst[i] == 'Z':
+            answer -= int(lst[i - 1])
+        else:
+            answer += int(lst[i])
+    
+    return answer
