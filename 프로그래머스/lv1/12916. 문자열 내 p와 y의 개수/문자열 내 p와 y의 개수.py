3cb5895e0ab20cb5bb399d2ab1ee1ca56f50c68c
조예지 <97578535+celpegor216@users.noreply.github.com>
2023-01-13 16:27:15 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
@@ -0,0 +1,11 @@
+def solution(s):
+    cnt_p = 0
+    cnt_y = 0
+    
+    for i in s:
+        if i == 'p' or i == 'P':
+            cnt_p += 1
+        elif i == 'y' or i == 'Y':
+            cnt_y += 1
+
+    return cnt_p == cnt_y
