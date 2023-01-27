df70ec302b0d1d48bcb29476b36d5bf5026f46df
조예지 <97578535+celpegor216@users.noreply.github.com>
2023-01-28 08:45:51 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -1,18 +1,3 @@
 def solution(skill, skill_trees):
-    answer = 0
-    
-    for skill_tree in skill_trees:
-        nxt = 0
-        
-        for s in skill_tree:
-            if s in skill:
-                if skill.index(s) == nxt:
-                    nxt += 1
-                else:
-                    nxt = -1
-                    break
-        
-        if nxt != -1:
-            answer += 1
-    
+    answer = -1
     return answer
