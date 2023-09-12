0500221e20cc25680d8931d442cd787f31c1478f
조예지 <97578535+celpegor216@users.noreply.github.com>
2023-09-13 07:38:03 +0900

    [Silver IV] Title: 카드 놓기, Time: 132 ms, Memory: 115268 KB -BaekjoonHub

---
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -0,0 +1,21 @@
+N = int(input())
+K = int(input())
+lst = [input() for _ in range(N)]
+
+used = [0] * N
+
+result = set()
+def dfs(level, now):
+    if level == K:
+        result.add(now)
+        return
+    
+    for n in range(N):
+        if not used[n]:
+            used[n] = 1
+            dfs(level + 1, now + lst[n])
+            used[n] = 0
+
+dfs(0, '')
+
+print(len(result))
