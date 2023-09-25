de3eded0c9000846e3aff74e94f5e23ae75b84bf
조예지 <97578535+celpegor216@users.noreply.github.com>
2023-09-26 08:46:38 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 128 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -0,0 +1,36 @@
+# 같은 문제: https://www.acmicpc.net/problem/1669
+
+# 값 1 => 최소 1, 최대 1
+# 값 2 => 최소 2, 최대 2
+# 값 3 => 최소 3, 최대 4
+# 값 4 => 최소 5, 최대 6
+# 값 5 => 최소 7, 최대 9
+# 값 6 => 최소 10, 최대 12
+# 값 7 => 최소 13, 최대 16
+# 값 8 => 최소 17, 최대 20
+
+# 값 2n => 최소 n ** 2 + 1, 최대 n(n+1)
+# 값 2n + 1 => 최소 n(n+1) + 1, 최대 (n+1) ** 2
+
+T = int(input())
+
+for t in range(T):
+    X, Y = map(int, input().split())
+
+    diff = Y - X
+
+    result = 0
+
+    if diff != result:
+        while 1:
+            if result ** 2 < diff <= result * (result + 1):
+                result = 2 * result
+                break
+            
+            if result * (result + 1) < diff <= (result + 1) ** 2:
+                result = 2 * result + 1
+                break
+
+            result += 1
+
+    print(result)
