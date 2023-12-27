8ec557471e4d19ea0769087d6875eeef9a396c70
조예지 <97578535+celpegor216@users.noreply.github.com>
2023-12-27 14:10:45 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 520 ms, Memory: 150692 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,15 @@
+# 해답: https://study-all-night.tistory.com/6
+
+N = int(input())
+
+def hanoi(n, start, end):
+    if n == 1:
+        print(start, end)
+        return
+    
+    hanoi(n - 1, start, 6 - start - end)
+    print(start, end)
+    hanoi(n - 1, 6 - start - end, end)
+
+print(2 ** N - 1)
+hanoi(N, 1, 3)
