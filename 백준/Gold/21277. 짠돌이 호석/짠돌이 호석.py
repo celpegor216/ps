23c0ee7e878f3c7f4cc793183e489ae2a3221ac7
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-04-03 11:40:16 +0900

    [Gold III] Title: 짠돌이 호석, Time: 508 ms, Memory: 112504 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21277. 짠돌이 호석/짠돌이 호석.py
+++ 백준/Gold/21277. 짠돌이 호석/짠돌이 호석.py
@@ -1,50 +1,37 @@
+# 해답: https://ongveloper.tistory.com/526
+
+board = [['0'] * 150 for _ in range(150)]
+
 N1, M1 = map(int, input().split())
 P1 = [input() for _ in range(N1)]
+
+for n in range(N1):
+    for m in range(M1):
+        board[50 + n][50 + m] = P1[n][m]
+
 N2, M2 = map(int, input().split())
 P2 = [input() for _ in range(N2)]
 
-result = max((N1 + N2) * max(M1, M2), (M1 + M2) * max(N1, N2))
+result = min((N1 + N2) * max(M1, M2), (M1 + M2) * max(N1, N2))
 
 for d in range(4):
-    for i in range(N1):
-        for j in range(M1):
-            if not (P1[i][j] == '1' and P2[0][0] == '1'):
-                flag = 0
+    for i in range(101):
+        for j in range(101):
+            flag = 0
 
-                for n in range(N2):
-                    if flag or i + n == N1:
+            for n in range(N2):
+                if flag:
+                    break
+
+                for m in range(M2):
+                    if board[i + n][j + m] == '1' and P2[n][m] == '1':
+                        flag = 1
                         break
-
-                    for m in range(M2):
-                        if j + m == M1:
-                            break
-
-                        if P1[i + n][j + m] == '1' and P2[n][m] == '1':
-                            flag = 1
-                            break
-                
-                if not flag:
-                    result = min(result, max(N1, N2 + i) * max(M1, M2 + j))
-
-    for i in range(N2):
-        for j in range(M2):
-            if not (P2[i][j] == '1' and P1[0][0] == '1'):
-                flag = 0
-
-                for n in range(N1):
-                    if flag or i + n == N2:
-                        break
-
-                    for m in range(M1):
-                        if j + m == M2:
-                            break
-
-                        if P2[i + n][j + m] == '1' and P1[n][m] == '1':
-                            flag = 1
-                            break
-                
-                if not flag:
-                    result = min(result, max(N2, N1 + i) * max(M2, M1 + j))
+            
+            if not flag:
+                width = max(50 + N1, i + N2) - min(i, 50)
+                height = max(50 + M1, j + M2) - min(j, 50)
+                result = min(result, width * height)
 
     next_P2 = [''] * M2
 
