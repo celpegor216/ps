497104cd52aec66024466b15ba3d146a18b3269c
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-04-03 11:09:51 +0900

    [Gold III] Title: 짠돌이 호석, Time: 192 ms, Memory: 112220 KB, Score: 15 point -BaekjoonHub

--- 백준/Gold/21277. 짠돌이 호석/짠돌이 호석.py
+++ 백준/Gold/21277. 짠돌이 호석/짠돌이 호석.py
@@ -26,6 +26,26 @@ for d in range(4):
                 if not flag:
                     result = min(result, max(N1, N2 + i) * max(M1, M2 + j))
 
+    for i in range(N2):
+        for j in range(M2):
+            if not (P2[i][j] == '1' and P1[0][0] == '1'):
+                flag = 0
+
+                for n in range(N1):
+                    if flag or i + n == N2:
+                        break
+
+                    for m in range(M1):
+                        if j + m == M2:
+                            break
+
+                        if P2[i + n][j + m] == '1' and P1[n][m] == '1':
+                            flag = 1
+                            break
+                
+                if not flag:
+                    result = min(result, max(N2, N1 + i) * max(M2, M1 + j))
+
     next_P2 = [''] * M2
 
     for m in range(M2):
