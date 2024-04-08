7cb6782477be7f99178eb616090b4327d36632e5
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-04-08 09:47:03 +0900

    [Bronze II] Title: 방 배정하기, Time: 112 ms, Memory: 108080 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.py
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -1,26 +1,16 @@
-rooms = list(map(int, input().split()))
-N = rooms[-1]
+tmp = list(map(int, input().split()))
+rooms = sorted(tmp[:3])
+N = tmp[-1]
 
-result = False
+dp = [[0] * (N + 1) for _ in range(3)]
 
-used = [0] * 3
+for i in range(3):
+    for j in range(N + 1):
+        if j < rooms[i]:
+            dp[i][j] = dp[i - 1][j]
+        elif j == rooms[i]:
+            dp[i][j] = 1
+        else:
+            dp[i][j] = 1 if dp[i][j - rooms[i]] or dp[i - 1][j] else 0
 
-def dfs(now):
-    global result
-
-    if result:
-        return
-    
-    if now == 0:
-        result = True
-        return
-    
-    for i in range(3):
-        if not used[i]:
-            used[i] = 1
-            dfs(now % rooms[i])
-            used[i] = 0
-
-dfs(N)
-
-print(1 if result else 0)
+print(dp[-1][-1])
