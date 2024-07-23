65e150a8d092dc8b2d6b8bc73af6d4463249ebb7
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-07-23 13:38:08 +0900

    [Gold IV] Title: 오렌지 출하, Time: 348 ms, Memory: 111124 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/11985. 오렌지 출하/오렌지 출하.py
+++ 백준/Gold/11985. 오렌지 출하/오렌지 출하.py
@@ -1,16 +1,26 @@
+# 70점...
+# 해답: https://limdongjin.github.io/problemsolving/boj_11985.html#%E1%84%89%E1%85%A9%E1%84%89%E1%85%B3%E1%84%8F%E1%85%A9%E1%84%83%E1%85%B3
+
+import sys
+input = sys.stdin.readline
+
 N, M, K = map(int, input().split())
-lst = [int(input()) for _ in range(N)]
+lst = [0] + [int(input()) for _ in range(N)]
 
-dp = [0, K]
+dp = [0] * (N + 1)
+dp[0] = 0
+dp[1] = K
 
-for n in range(1, N):
-    tmp = dp[-1] + 21e10
-    for m in range(M):
-        if n < m:
-            break
+for n in range(2, N + 1):
+    dp[n] = dp[n - 1] + K
+    minv = maxv = lst[n]
 
-        tmp = min(tmp, dp[-m - 1] + K + (m + 1) * (max(lst[n - m:n + 1]) - min(lst[n - m:n + 1])))
+    for m in range(2, min(M, n) + 1):
+        i = n - m + 1
 
-    dp.append(tmp)
+        minv = min(minv, lst[i])
+        maxv = max(maxv, lst[i])
+
+        dp[n] = min(dp[n], dp[i - 1] + K + m * (maxv - minv))
 
 print(dp[-1])
