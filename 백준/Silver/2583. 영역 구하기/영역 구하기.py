fc869c2f06d9f46d27d54b71b21f0a740c043fb7
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-07-30 13:02:50 +0900

    [Silver I] Title: 영역 구하기, Time: 148 ms, Memory: 111644 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,41 +1,40 @@
-from collections import deque
-
-M, N, K = map(int, input().split())
+N, M, K = map(int, input().split())
 lst = [[0] * M for _ in range(N)]
 
-for k in range(K):
-    x1, y1, x2, y2 = map(int, input().split())
+for _ in range(K):
+    l, u, r, d = map(int, input().split())    # 주어진 영역을 가로축 기준 뒤집기
 
-    for x in range(x1, x2):
-        for y in range(y1, y2):
-            lst[x][y] = 1
+    for i in range(u, d):
+        for j in range(l, r):
+            lst[i][j] = 1
 
+result = []
 used = [[0] * M for _ in range(N)]
-
-results = []
 
 for n in range(N):
     for m in range(M):
         if not lst[n][m] and not used[n][m]:
-            q = deque()
-            q.append((n, m))
-
+            total = 1
             used[n][m] = 1
+            stack = []
+            now = (n, m)
 
-            cnt = 0
+            while 1:
+                for dy, dx in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+                    ny, nx = now[0] + dy, now[1] + dx
 
-            while q:
-                x, y = q.popleft()
+                    if 0 <= ny < N and 0 <= nx < M and not lst[ny][nx] and not used[ny][nx]:
+                        total += 1
+                        used[ny][nx] = 1
+                        stack.append(now)
+                        now = (ny, nx)
+                        break
+                else:
+                    if stack:
+                        now = stack.pop()
+                    else:
+                        break
+            result.append(total)
 
-                cnt += 1
-
-                for dx, dy in ((0, 1), (1, 0), (0, -1), (-1, 0)):
-                    nx, ny = x + dx, y + dy
-                    if 0 <= nx < N and 0 <= ny < M and not lst[nx][ny] and not used[nx][ny]:
-                        q.append((nx, ny))
-                        used[nx][ny] = 1
-            
-            results.append(cnt)
-
-print(len(results))
-print(*sorted(results))
+print(len(result))
+print(*sorted(result))
