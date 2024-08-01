2bea165fc7bcde1fd3d7425fddf9147c57c5db10
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-01 09:33:52 +0900

    [Gold V] Title: 적록색약, Time: 156 ms, Memory: 114096 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,48 +1,35 @@
 from collections import deque
 
 N = int(input())
-
 lst = [input() for _ in range(N)]
-used = [[0] * N for _ in range(N)]
 
-r = 0
-g = 0
-b = 0
-rg = 0
+def bfs(is_red_equals_green):
+    used = [[0] * N for _ in range(N)]
+    cnt = 0
 
-def bfs(y, x, color):
-    q = deque()
-    q.append((y, x))
+    if is_red_equals_green:
+        colors = {'R': 'RG', 'G': 'RG', 'B': 'B'}
+    else:
+        colors = {'R': 'R', 'G': 'G', 'B': 'B'}
 
-    while q:
-        nowy, nowx = q.popleft()
+    for i in range(N):
+        for j in range(N):
+            if not used[i][j]:
+                q = deque()
+                q.append((i, j))
+                used[i][j] = 1
+                cnt += 1
 
-        for dy, dx in ((0, 1), (1, 0), (0, -1), (-1, 0)):
-            ny, nx = nowy + dy, nowx + dx
-            if 0 <= ny < N and 0 <= nx < N and not used[ny][nx] and lst[ny][nx] in color:
-                q.append((ny, nx))
-                used[ny][nx] = 1
+                while q:
+                    y, x = q.popleft()
 
-for i in range(N):
-    for j in range(N):
-        if not used[i][j]:
-            if lst[i][j] == 'R':
-                r += 1
-                bfs(i, j, 'R')
-            elif lst[i][j] == 'G':
-                g += 1
-                bfs(i, j, 'G')
-            elif lst[i][j] == 'B':
-                b += 1
-                bfs(i, j, 'B')
+                    for dy, dx in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+                        ny, nx = y + dy, x + dx
 
-used = [[0] * N for _ in range(N)]
+                        if 0 <= ny < N and 0 <= nx < N and not used[ny][nx] and lst[ny][nx] in colors[lst[i][j]]:
+                            q.append((ny, nx))
+                            used[ny][nx] = 1
 
-for i in range(N):
-    for j in range(N):
-        if not used[i][j]:
-            if lst[i][j] in ('R', 'G'):
-                rg += 1
-                bfs(i, j, ('R', 'G'))
+    return cnt
 
-print(r + g + b, rg + b)
+print(bfs(0), bfs(1))
