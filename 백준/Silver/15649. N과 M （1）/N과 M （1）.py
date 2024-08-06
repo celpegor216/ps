79f1b00ae4373b7011e434a1f7fbfc1512f7edc2
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-06 13:02:35 +0900

    [Silver III] Title: N과 M (1), Time: 168 ms, Memory: 115192 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,16 +1,15 @@
 N, M = map(int, input().split())
 
-used = [0] * (N + 1)
-
-def dfs(level, path):
+used = [0] * N
+def dfs(level, now):
     if level == M:
-        print(*path)
+        print(*now)
         return
-    
-    for i in range(1, N + 1):
+
+    for i in range(N):
         if not used[i]:
             used[i] = 1
-            dfs(level + 1, path + [i])
+            dfs(level + 1, now + [i + 1])
             used[i] = 0
 
 dfs(0, [])
