814246d8da37b6c3362df2bb70ba40811fe04cd9
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-06 13:04:35 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,11 +1,11 @@
 N, M = map(int, input().split())
 
-def dfs(level, start, path):
+def dfs(level, start, now):
     if level == M:
-        print(*path)
+        print(*now)
         return
 
-    for i in range(start + 1, N + 1):
-        dfs(level + 1, i, path + [i])
+    for i in range(start, N):    # 중복 없이 M개를 고르고, 고른 수열은 오름차순
+        dfs(level + 1, i + 1, now + [i + 1])
 
 dfs(0, 0, [])
