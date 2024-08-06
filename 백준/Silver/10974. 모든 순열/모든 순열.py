6be55b0f49f7a2885ac5bfbbd35959a4cc113aa5
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-06 13:06:44 +0900

    [Silver III] Title: 모든 순열, Time: 176 ms, Memory: 115212 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.py
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -1,16 +1,16 @@
 N = int(input())
 
-used = [0] * N
+used = [0] * N    # 1부터 N까지의 수로 이루어진 순열
 
-def dfs(level, path):
+def dfs(level, now):
     if level == N:
-        print(*path)
+        print(*now)
         return
-    
-    for i in range(N):
+
+    for i in range(N):    # 사전순
         if not used[i]:
             used[i] = 1
-            dfs(level + 1, path + [i + 1])
+            dfs(level + 1, now + [i + 1])
             used[i] = 0
 
 dfs(0, [])
