74690e614de3fa566311f641f732dbc4b80667fd
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-12 13:28:30 +0900

    [Bronze II] Title: 수 정렬하기, Time: 108 ms, Memory: 110976 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,10 +1,39 @@
+# 퀵 정렬 설명 참고
+
 N = int(input())
 lst = [int(input()) for _ in range(N)]
 
-for i in range(N):
-    for j in range(i + 1, N):
-        if lst[i] > lst[j]:
-            lst[i], lst[j] = lst[j], lst[i]
+def quick_sort(s, e):
+    if s >= e:
+        return
 
-for item in lst:
-    print(item)
+    pivot = lst[s]
+    left = s + 1
+    right = e
+
+    while left < right:
+        while left <= e and lst[left] < pivot:
+            left += 1
+
+        while right > s and lst[right] >= pivot:
+            right -= 1
+
+        if left > right:
+            left = right
+            break
+
+        if left != right:
+            lst[left], lst[right] = lst[right], lst[left]
+
+    if lst[left] > pivot:
+        lst[left - 1], lst[s] = lst[s], lst[left - 1]
+        quick_sort(s, left - 2)
+        quick_sort(left, e)
+    else:
+        lst[left], lst[s] = lst[s], lst[left]
+        quick_sort(s, left - 1)
+        quick_sort(left + 1, e)
+
+quick_sort(0, N - 1)
+
+print(*lst)
