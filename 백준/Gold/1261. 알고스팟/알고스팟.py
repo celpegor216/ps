db0c4b0e2fb7111c278766107b4b751f522db236
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-12 22:57:02 +0900

    [Gold IV] Title: 알고스팟, Time: 152 ms, Memory: 112560 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -1,28 +1,31 @@
-from collections import deque
+import heapq
 
 M, N = map(int, input().split())
 lst = [input() for _ in range(N)]
 
-used = [[[21e8, 21e8] for _ in range(M)] for _ in range(N)]
-used[0][0] = [0, 0]
+q = []
+heapq.heappush(q, (0, 0, 0))
 
-q = deque()
-q.append((0, 0, 0))
+result = N * M
+used = [[result] * M for _ in range(N)]
+used[0][0] = 0
 
 while q:
-    y, x, cnt = q.popleft()
+    cnt, y, x = heapq.heappop(q)
+
+    if used[y][x] < cnt:
+        continue
 
     if y == N - 1 and x == M - 1:
+        result = min(result, cnt)
         continue
 
     for dy, dx in ((0, 1), (1, 0), (0, -1), (-1, 0)):
         ny, nx = y + dy, x + dx
         if 0 <= ny < N and 0 <= nx < M:
-            if lst[ny][nx] == '0' and used[ny][nx][0] > cnt:
-                used[ny][nx][0] = cnt
-                q.append((ny, nx, cnt))
-            elif lst[ny][nx] == '1' and used[ny][nx][1] > cnt + 1:
-                used[ny][nx][1] = cnt + 1
-                q.append((ny, nx, cnt + 1))
+            nxt_cnt = cnt + 1 if lst[ny][nx] == '1' else cnt
+            if used[ny][nx] > nxt_cnt:
+                used[ny][nx] = nxt_cnt
+                heapq.heappush(q, (nxt_cnt, ny, nx))
 
-print(min(used[-1][-1]))
+print(result)
