9d0f74c706cbb25af42c4721a4cb778378dbc8ed
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-13 13:16:14 +0900

    [Silver II] Title: 연속합, Time: 132 ms, Memory: 120816 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,13 +1,15 @@
-# 해답: https://bio-info.tistory.com/160
-
 N = int(input())
 lst = list(map(int, input().split()))
 
-dp = [lst[0]]
+result = now = lst[0]
 
 for n in range(1, N):
-    # dp[-1] + lst[n]: 이전 원소를 포함한 경우 최댓값
-    # lst[n]: 이전 원소를 포함하지 않은 경우 최댓값
-    dp.append(max(dp[-1] + lst[n], lst[n]))
+    # 지금까지 더한 합(now)에 지금 것을 더한 것보다 지금 것의 값이 더 큰 경우
+    # 아예 now를 쓰지 않는 것이 좋음
+    if now + lst[n] < lst[n]:
+        now = lst[n]
+    else:
+        now += lst[n]
+    result = max(result, now)
 
-print(max(dp))
+print(result)
