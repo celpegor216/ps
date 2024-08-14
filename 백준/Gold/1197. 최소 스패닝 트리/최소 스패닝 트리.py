6e29b0bb8ef424171c805ad00cd61519a10f6c98
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-14 14:43:49 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 424 ms, Memory: 127320 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,32 +1,37 @@
-# 크루스칼
-# 구현은 해답 참조함
-
-V, E = map(int, input().split())
-edges = [list(map(int, input().split())) for _ in range(E)]
-edges.sort(key=lambda x: x[2])
-
-group = [i for i in range(V + 1)]
+import sys, heapq
+input = sys.stdin.readline
 
 def find(a):
     if group[a] != a:
         group[a] = find(group[a])
     return group[a]
 
-def union(y, x, e):
-    global total, cnt
-    group_y, group_x = find(y), find(x)
-    
-    if group_y != group_x:
-        total += edges[e][2]
-        
-        if group_x < group_y:
-            group[group_y] = group_x
-        else:
-            group[group_x] = group_y
-            
+def union(a, b):
+    group_a, group_b = find(a), find(b)
 
-total = 0
-for e in range(E):
-    union(edges[e][0], edges[e][1], e)
-    
-print(total)
+    if group_a != group_b:
+        if group_a > group_b:
+            group_a, group_b = group_b, group_a
+        group[group_b] = group_a
+        return 1
+    return 0
+
+V, E = map(int, input().split())
+group = [v for v in range(V + 1)]
+q = []
+
+for _ in range(E):
+    a, b, c = map(int, input().split())
+    heapq.heappush(q, (c, a, b))
+
+result = 0
+cnt = V - 1
+
+while cnt:
+    c, a, b = heapq.heappop(q)
+
+    if union(a, b):
+        result += c
+        cnt -= 1
+
+print(result)
