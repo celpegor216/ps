aa1e700a39572a5eea367aa6a51a77d5c56500b6
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-16 10:22:14 +0900

    [Gold V] Title: 최소비용 구하기, Time: 228 ms, Memory: 121880 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,35 +1,34 @@
-# 풀이 참조
-
-import heapq
-import sys
+import heapq, sys
 input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
+lst = [[] for _ in range(N + 1)]    # 도시의 번호가 1부터 N까지이므로 N + 1
 
-graph = [[] for _ in range(N + 1)]
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    lst[a].append((b, c))    # 양방향이 아닌 단방향 그래프
 
-for m in range(M):
-    s, e, c = map(int, input().split())
+S, E = map(int, input().split())
 
-    graph[s].append((e, c))
+q = []
+heapq.heappush(q, (0, S))
 
-start, end = map(int, input().split())
-INF = 1e8
-result = [INF] * (N + 1) # 시작 도시부터 index 도시까지의 비용, 바로 갈 수 없는 경우 INF
-result[start] = 0
+# 출발부터 도착까지 모든 도시를 거쳐서 간다고 해도 1000(도시 개수)*100000(최대 비용)
+# 따라서 최대값을 21 * 10 ** 8으로 지정하면 안전함
+results = [int(21e8)] * (N + 1)
+results[S] = 0
 
-pq = []
-heapq.heappush(pq, (0, start)) # (시작 도시부터 경유 도시까지의 비용, 경유 도시)
+while q:
+    cost, via = heapq.heappop(q)
+    
+    if results[via] < cost:
+        continue
 
-while pq:
-    start_via_cost, via = heapq.heappop(pq)
+    for nxt, nxt_cost in lst[via]:
+        if results[nxt] > cost + nxt_cost:
+            results[nxt] = cost + nxt_cost
+            heapq.heappush(q, (cost + nxt_cost, nxt))
 
-    if start_via_cost <= result[via]:
-        for item in graph[via]:
-            start_via_target_cost = start_via_cost + item[1]
-            if start_via_target_cost < result[item[0]]:
-                result[item[0]] = start_via_target_cost
-                heapq.heappush(pq, (start_via_target_cost, item[0]))
-
-print(result[end])
+# 출발점에서 도착점을 갈 수 있는 경우만 주어지므로 도달 가능 여부를 확인하지 않아도 됨
+print(results[E])
