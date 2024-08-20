ad13e6b14a1000849e9c141df30140fc0d0190b0
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-20 15:19:10 +0900

    [Silver IV] Title: ZOAC 3, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/20436. ZOAC 3/ZOAC 3.py
+++ 백준/Silver/20436. ZOAC 3/ZOAC 3.py
@@ -1,26 +1,40 @@
-l, r = input().split()
+# 자음은 왼손으로만, 모음은 오른손으로만... 문제를 제대로 읽지 않았음
 
-lst = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
-dic = dict()
+
+keyboards = [
+    'qwertyuiop',
+    'asdfghjkl',
+    'zxcvbnm'
+]
+
+# 키보드의 각 줄에서 모음이 시작되는 위치
+limits = [5, 5, 4]
+
+left_pos = dict()
+right_pos = dict()
 
 for i in range(3):
-    for j in range(len(lst[i])):
-        dic[lst[i][j]] = (i, j)
+    for j in range(len(keyboards[i])):
+        if j >= limits[i]:
+            right_pos[keyboards[i][j]] = (i, j)
+        else:
+            left_pos[keyboards[i][j]] = (i, j)
+
+left, right = input().split()
+S = input()
+N = len(S)
 
 result = 0
 
-left = 'qwertasdfgzxcv'
-
-S = input()
-
 for s in S:
-    if s in left:
-        result += abs(dic[l][0] - dic[s][0]) + abs(dic[l][1] - dic[s][1])
-        l = s
-    else:
-        result += abs(dic[r][0] - dic[s][0]) + abs(dic[r][1] - dic[s][1])
-        r = s
-    
-    result += 1
+    if left == s or right == s:
+        continue
 
-print(result)
+    if s in left_pos:
+        result += abs(left_pos[left][0] - left_pos[s][0]) + abs(left_pos[left][1] - left_pos[s][1])
+        left = s
+    else:
+        result += abs(right_pos[right][0] - right_pos[s][0]) + abs(right_pos[right][1] - right_pos[s][1])
+        right = s
+
+print(result + len(S))
