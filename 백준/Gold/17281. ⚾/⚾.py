59afb4eb3b467c7c39708ae4801c5104c59dd1d5
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-22 19:25:18 +0900

    [Gold IV] Title: ⚾, Time: 1072 ms, Memory: 112692 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.py
+++ 백준/Gold/17281. ⚾/⚾.py
@@ -1,55 +1,52 @@
-# 완전 탐색하면 시간초과인데 완전 탐색을 하지 않고 최고 득점을 찾는 방법을 모르겠음
-# 해답: https://edder773.tistory.com/60
-# 완전 탐색은 맞고 득점을 구하는 부분에서 시간을 줄여야 했음
-
-N = int(input())
+N = int(input())    # 이닝 수
 lst = [list(map(int, input().split())) for _ in range(N)]
 
-used = [0] * 9
-used[0] = 4
-result = 0
-pluses = {1: 1, 2: 11, 3: 111, 4: 1111}
+M = 9
+# 1번 선수를 4번 타자로 미리 결정했다
 
-def dfs(level):
+result = 0
+used = [0] * M
+def dfs(level, order):
     global result
 
-    if level == 9:
-        order = [used.index(x) for x in range(1, 10)]
-        total = 0
-        now = 0
+    if level == M:
+        bases = [-1] * M    # i번째 타석에 위치한 선수의 인덱스
+        for i in range(M):
+            bases[order[i]] = i
 
-        for n in range(N):
-            out = 0
-            b1 = b2 = b3 = 0
-
+        total = 0    # 이 배치대로 수행했을 때 얻을 수 있는 최대 점수
+        now = 0    # 현재 나와있는 선수의 인덱스
+        for n in range(N):    # 이닝 시작
+            b1 = b2 = b3 = 0    # 현재 베이스 상태, 비어있으면 0
+            out = 0    # 현재 이닝의 아웃 카운트
             while out < 3:
-                if lst[n][order[now]] == 0:
+                if lst[n][bases[now]] == 0:
                     out += 1
-                elif lst[n][order[now]] == 1:
+                elif lst[n][bases[now]] == 1:
                     total += b3
                     b1, b2, b3 = 1, b1, b2
-                elif lst[n][order[now]] == 2:
+                elif lst[n][bases[now]] == 2:
                     total += b3 + b2
                     b1, b2, b3 = 0, 1, b1
-                elif lst[n][order[now]] == 3:
+                elif lst[n][bases[now]] == 3:
                     total += b3 + b2 + b1
                     b1, b2, b3 = 0, 0, 1
-                elif lst[n][order[now]] == 4:
+                else:
                     total += b3 + b2 + b1 + 1
                     b1 = b2 = b3 = 0
-                now += 1
-                if now == 9:
-                    now = 0
+                now = (now + 1) % 9
 
-            result = max(result, total)
+        result = max(result, total)
         return
 
-    for i in range(1, 10):
-        if i not in used:
-            used[level] = i
-            dfs(level + 1)
-            used[level] = 0
+    # level번째 선수를 i번째 타석에 배치
+    for i in range(M):
+        if not used[i]:
+            used[i] = 1
+            dfs(level + 1, order + [i])
+            used[i] = 0
 
-dfs(1)
+used[3] = 1
+dfs(1, [3])
 
 print(result)
