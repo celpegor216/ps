84e6a2125dcf7df6fc8634b16d77b7e714b20f92
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-24 16:20:13 +0900

    [Bronze IV] Title: 가희와 클럽 오디션 1, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/30794. 가희와 클럽 오디션 1/가희와 클럽 오디션 1.py
@@ -0,0 +1,12 @@
+A, B = input().split()
+
+if B == 'miss':
+    print(0)
+elif B == 'bad':
+    print(int(A) * 200)
+elif B == 'cool':
+    print(int(A) * 400)
+elif B == 'great':
+    print(int(A) * 600)
+elif B == 'perfect':
+    print(int(A) * 1000)
