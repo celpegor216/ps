d65af26bc2aafc4a2eab6ededa94a0f0b3949508
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-26 17:40:55 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
@@ -1,11 +1,21 @@
 N = int(input())
-dic = dict()
+M = 10    # 소의 번호 최댓값
+
+# last_position[i]: i번째 소가 가장 마지막으로 길의 어느쪽에서 관찰되었는지 저장
+# 소의 번호가 1 ~ 10까지이므로 M + 1
+# 초기에는 어느 위치에 있는지 모르기 때문에 -1
+last_position = [-1] * (M + 1)
 
 result = 0
-for n in range(N):
-    num, flag = map(int, input().split())
-    if dic.get(num) in (0, 1) and dic[num] != flag:
+for _ in range(N):
+    i, j = map(int, input().split())
+
+    # i번 소가 처음으로 관찰된 경우
+    if last_position[i] == -1:
+        last_position[i] = j
+    # i번 소가 이미 관찰된 적이 있으면서 지금과 다른 위치에 있는 경우, 이동한 것
+    elif last_position[i] != j:
+        last_position[i] = j
         result += 1
-    dic[num] = flag
 
 print(result)
