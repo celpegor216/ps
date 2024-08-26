dbfe4c99a22f1e06ce14b401372d3076a0b13926
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-26 20:44:52 +0900

    [Silver V] Title: 단어 나누기, Time: 92 ms, Memory: 110444 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.py
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -1,12 +1,18 @@
+# 파이썬에서는 연산자를 사용해서 문자열이 사전순으로 앞서는지 비교할 수 있음
+
 S = input()
-length = len(S)
+N = len(S)
 
-result = 'z' * length
+# 어떻게 글자를 나누어서 뒤집더라도 그것보다 사전순으로 뒤에 나오는 글자로 초기화
+result = 'z' * N
 
-for i in range(1, length - 1):
-    for j in range(i + 1, length):
-        temp = S[:i][::-1] + S[i:j][::-1] + S[j:][::-1]
+# 첫 번재 나누는 지점
+for i in range(1, N - 1):
+    # 두 번째 나누는 지점
+    for j in range(i + 1, N):
+        tmp = S[:i][::-1] + S[i:j][::-1] + S[j:][::-1]
 
-        result = min(result, temp)
+        if result > tmp:
+            result = tmp
 
 print(result)
