5a105117a5595550b7709bf7bdc8d8d39be8fb8f
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-26 20:38:13 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
@@ -1,44 +1,48 @@
-vowels = 'aeiou'
+# 조건에 하나라도 해당하지 않을 경우 바로 반환하기 위해 함수 사용
+def check(S):
+    vowels = 'aeiou'
+
+    flag = 0
+
+    # 모음(a,e,i,o,u) 하나를 반드시 포함하여야 한다.
+    for vowel in vowels:
+        if vowel in S:
+            flag = 1
+
+    if not flag:
+        return
+
+    N = len(S)
+    char_types = [0] * N
+    for n in range(N):
+        # 같은 글자가 연속적으로 두번 오면 안되나, ee 와 oo는 허용한다.
+        if n < N - 1 and S[n] not in 'eo' and S[n] == S[n + 1]:
+            return
+        if S[n] in vowels:
+            char_types[n] = 1
+        else:
+            char_types[n] = 0
+
+    # 모음이 3개 혹은 자음이 3개 연속으로 오면 안 된다.
+    cnt = 0
+    now = -1
+    for n in range(N):
+        if char_types[n] == now:
+            cnt += 1
+        else:
+            cnt = 1
+            now = char_types[n]
+
+        if cnt > 2:
+            return
+
+    return 1
+
 
 while 1:
-    s = input()
+    S = input()
 
-    if s == 'end':
+    if S == 'end':
         break
 
-    check_vowel = 0
-    
-    for vowel in vowels:
-        if vowel in s:
-            check_vowel = 1
-            break
-    
-    flag = 0
-    if not check_vowel:
-        flag = 1
-    else:
-        now = ''
-        now_cnt = 0
-        type_cnt = 0
-
-        for i in range(len(s)):
-            if s[i] == now:
-                now_cnt += 1
-                type_cnt += 1
-            elif (s[i] in vowels and now in vowels) or (s[i] not in vowels and now not in vowels):
-                now = s[i]
-                now_cnt = 1
-                type_cnt += 1
-            else:
-                now = s[i]
-                now_cnt = 1
-                type_cnt = 1
-            
-            if (now not in 'eo' and now_cnt > 1) or type_cnt > 2:
-                flag = 1
-                break
-    
-    if flag:
-        print(f'<{s}> is not acceptable.')
-    else:
-        print(f'<{s}> is acceptable.')
+    print(f'<{S}> is acceptable.' if check(S) else f'<{S}> is not acceptable.')
