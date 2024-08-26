2be9671c751f6f24d6711103c6e81832917de506
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-26 20:06:30 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 88 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,23 +1,28 @@
 N = int(input())
 
 result = 0
-for n in range(N):
+for _ in range(N):
     S = input()
 
-    now = S[0]
-    used = [now]
+    # check[i]: ord('a') + i 번째 글자가 등장했는지 여부
+    check = [0] * 26
+    # 현재 이전의 글자
+    now = ''
 
-    flag = 0
-    for i in range(1, len(S)):
-        if S[i] != now:
-            if S[i] in used:
-                flag = 1
+    for s in S:
+        # 글자가 이전과 달라졌을 경우
+        if now != s:
+            idx = ord(s) - ord('a')
+
+            # 현재 글자가 이전에 등장한 적이 있는 경우,
+            # 모든 문자가 연속해서 나타난 것이 아니므로
+            # 이 단어는 그룹 단어가 아님
+            if check[idx]:
                 break
-            else:
-                now = S[i]
-                used.append(now)
-    
-    if not flag:
+
+            now = s
+            check[idx] = 1
+    else:
         result += 1
 
 print(result)
