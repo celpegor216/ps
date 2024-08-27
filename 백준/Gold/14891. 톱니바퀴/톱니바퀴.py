bb9f0a9ae3f99b1d3d8eb4906259582af768e7e5
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-27 15:49:22 +0900

    [Gold V] Title: 톱니바퀴, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,48 +1,48 @@
-# 0번의 2 - 1번의 6
-# 1번의 2 - 2번의 6
-# 2번의 2 - 3번의 6
+N = 4
+# 12시 방향부터 시계 방향 순서대로
+# n번 톱니바퀴의 2번 - n + 1번 톱니바퀴의 6번
+lst = [input() for _ in range(N)]
 
-# 시계 방향으로 회전 > 오른쪽 1칸 이동
-# 반시계 방향으로 회전 > 왼쪽 1칸 이동
-
-gears = [input() for _ in range(4)]
+# check_order[i]: i번째 톱니바퀴를 회전시킬 때 회전하는지 확인해봐야 하는 톱니바퀴 순서쌍
+check_order = [
+    [[0, 1], [1, 2], [2, 3]],
+    [[1, 0], [1, 2], [2, 3]],
+    [[2, 1], [2, 3], [1, 0]],
+    [[3, 2], [2, 1], [1, 0]]
+]
 
 K = int(input())
 
-for k in range(K):
-    idx, d = map(int, input().split())
-    idx -= 1
+for _ in range(K):
+    # 첫 번째 정수는 회전시킨 톱니바퀴의 번호, 두 번째 정수는 방향
+    # 1은 시계방향, -1은 반시계 방향
+    n, d = map(int, input().split())
+    n -= 1
 
-    move = [0] * 4
-    move[idx] = d
+    will_rotate = [0] * N
+    will_rotate[n] = d
 
-    left = idx - 1
-    right = idx + 1
-
-    while 0 <= left < 4:
-        if gears[left][2] != gears[left + 1][6]:
-            move[left] = move[left + 1] * -1
-            left -= 1
+    # 톱니바퀴 A를 회전할 때, 그 옆에 있는 톱니바퀴 B와 서로 맞닿은 톱니의 극이 다르다면,
+    # B는 A가 회전한 방향과 반대방향으로 회전하게 된다
+    for now, nxt in check_order[n]:
+        # now의 2번과 nxt의 6번 확인
+        if now < nxt:
+            if lst[now][2] != lst[nxt][6]:
+                will_rotate[nxt] = will_rotate[now] * -1
+        # now의 6번과 nxt의 2번 확인
         else:
-            break
-    
-    while 0 <= right < 4:
-        if gears[right][6] != gears[right - 1][2]:
-            move[right] = move[right - 1] * -1
-            right += 1
-        else:
-            break
-    
-    for i in range(4):
-        if move[i] == 1:
-            gears[i] = gears[i][-1] + gears[i][:-1]
-        elif move[i] == -1:
-            gears[i] = gears[i][1:] + gears[i][0]
-    
+            if lst[now][6] != lst[nxt][2]:
+                will_rotate[nxt] = will_rotate[now] * -1
+
+    for n in range(N):
+        if will_rotate[n] == 1:
+            lst[n] = lst[n][-1] + lst[n][:-1]
+        elif will_rotate[n] == -1:
+            lst[n] = lst[n][1:] + lst[n][0]
+
+# 각 톱니바퀴의 12시 방향이 S극(1)인지 여부
 result = 0
-
-for i in range(4):
-    if gears[i][0] == '1':
-        result += 2 ** i
-
+for n in range(N):
+    if lst[n][0] == '1':
+        result += 2 ** n
 print(result)
