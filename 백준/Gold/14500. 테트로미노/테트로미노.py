2b7a7b9741e4c4291f713179dfba3436312c7a5f
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-27 15:45:01 +0900

    [Gold IV] Title: 테트로미노, Time: 452 ms, Memory: 113408 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,25 +1,45 @@
-N, M = map(int, input().split())
+# 테트로미노가 놓인 칸에 쓰여 있는 수들의 합을 최대
+# 회전이나 대칭을 시켜도 된다
 
-lst = [list(map(int, input().split(' '))) for _ in range(N)]
+N, M = map(int, input().split())
+lst = [list(map(int, input().split())) for _ in range(N)]
+
+# 뒤집기 포함
+blocks = [
+    [[1, 1, 1, 1]],
+    [[1], [1], [1], [1]],
+    [[1, 1], [1, 1]],
+    [[1, 0], [1, 0], [1, 1]],
+    [[0, 1], [0, 1], [1, 1]],
+    [[1, 0], [1, 1], [0, 1]],
+    [[0, 1], [1, 1], [1, 0]],
+    [[1, 0], [1, 1], [1, 0]]
+]
+
+rotate_cnts = [1, 1, 1, 4, 4, 2, 2, 4]
 
 result = 0
 
-def dfs(level, total, path):
-    global result
+# 블럭 별로
+for idx in range(len(blocks)):
+    now = blocks[idx]
+    a, b = len(now), len(now[0])
 
-    if level == 4:
-        if result < total:
-            result = total
-        return
+    # 돌릴 필요가 있다면 한 번씩 돌려가면서
+    for _ in range(rotate_cnts[idx]):
+        # 모든 좌표 탐색
+        for i in range(N - a + 1):
+            for j in range(M - b + 1):
+                total = 0
 
-    for y, x in path:
-        for dy, dx in ((0, 1), (1, 0), (0, -1), (-1, 0)):
-            ny, nx = y + dy, x + dx
-            if 0 <= ny < N and 0 <= nx < M and (ny, nx) not in path:
-                dfs(level + 1, total + lst[ny][nx], path + [(ny, nx)])
+                for y in range(a):
+                    for x in range(b):
+                        total += lst[i + y][j + x] * now[y][x]
 
-for n in range(N):
-    for m in range(M):
-        dfs(1, lst[n][m], [(n, m)])
+                result = max(result, total)
+
+        # 블록 시계 방향으로 돌리기
+        a, b = b, a
+        now = list(map(list, zip(*now[::-1])))
 
 print(result)
