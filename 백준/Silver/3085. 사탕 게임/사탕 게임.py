f1a4da1e25aa1e075993e81c75cb9d1f5741e92a
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-27 21:31:07 +0900

    [Silver II] Title: 사탕 게임, Time: 128 ms, Memory: 111424 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,78 +1,50 @@
 N = int(input())
 lst = [list(input()) for _ in range(N)]
 
-result = 0
+result = 1
 
+def check_vertical(j):
+    global result
+
+    cnt = 0
+    now = ''
+    for y in range(N):
+        if lst[y][j] != now:
+            now = lst[y][j]
+            cnt = 1
+        else:
+            cnt += 1
+            result = max(result, cnt)
+
+def check_horizontal(i):
+    global result
+
+    cnt = 0
+    now = ''
+    for x in range(N):
+        if lst[i][x] != now:
+            now = lst[i][x]
+            cnt = 1
+        else:
+            cnt += 1
+            result = max(result, cnt)
+
+# 가로로 바꾸고 확인
 for i in range(N):
-    flag = 0
     for j in range(N - 1):
-        if lst[i][j] != lst[i][j + 1]:
-            flag = 1
-            lst[i][j], lst[i][j + 1] = lst[i][j + 1], lst[i][j]
+        lst[i][j], lst[i][j + 1] = lst[i][j + 1], lst[i][j]
+        check_vertical(j)
+        check_vertical(j + 1)
+        check_horizontal(i)
+        lst[i][j], lst[i][j + 1] = lst[i][j + 1], lst[i][j]
 
-            rowc, colc, col2c = 1, 1, 1
-
-            for k in range(1, N):
-                if lst[i][k] != lst[i][k - 1]:
-                    result = max(result, rowc)
-                    rowc = 1
-                else:
-                    rowc += 1
-
-                if lst[k][j] != lst[k - 1][j]:
-                    result = max(result, colc)
-                    colc = 1
-                else:
-                    colc += 1
-
-                if lst[k][j + 1] != lst[k - 1][j + 1]:
-                    result = max(result, col2c)
-                    col2c = 1
-                else:
-                    col2c += 1
-
-            result = max(result, rowc, colc, col2c)
-
-            lst[i][j], lst[i][j + 1] = lst[i][j + 1], lst[i][j]
-    
-    if not flag:
-        result = N
-        break
-
-if result < N:
-    for j in range(N):
-        flag = 0
-        for i in range(N - 1):
-            if lst[i][j] != lst[i + 1][j]:
-                flag = 1
-                lst[i][j], lst[i + 1][j] = lst[i + 1][j], lst[i][j]
-
-                rowc, colc, row2c = 1, 1, 1
-
-                for k in range(1, N):
-                    if lst[i][k] != lst[i][k - 1]:
-                        result = max(result, rowc)
-                        rowc = 1
-                    else:
-                        rowc += 1
-
-                    if lst[k][j] != lst[k - 1][j]:
-                        result = max(result, colc)
-                        colc = 1
-                    else:
-                        colc += 1
-
-                    if lst[i + 1][k] != lst[i + 1][k - 1]:
-                        result = max(result, row2c)
-                        row2c = 1
-                    else:
-                        row2c += 1
-
-                result = max(result, rowc, colc, row2c)
-
-                lst[i][j], lst[i + 1][j] = lst[i + 1][j], lst[i][j]
-        
-        if not flag:
-            break
+# 세로로 바꾸고 확인
+for j in range(N):
+    for i in range(N - 1):
+        lst[i][j], lst[i + 1][j] = lst[i + 1][j], lst[i][j]
+        check_vertical(j)
+        check_horizontal(i)
+        check_horizontal(i + 1)
+        lst[i][j], lst[i + 1][j] = lst[i + 1][j], lst[i][j]
 
 print(result)
