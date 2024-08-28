4f56c6975fa8fce42e77871b398e3ccdce4a24d7
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-28 16:26:18 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 320 ms, Memory: 112032 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,91 +1,85 @@
-R, C, T = map(int, input().split())
+N, M, T = map(int, input().split())
+lst = [list(map(int, input().split())) for _ in range(N)]
+airs = []
 
-lst = [list(map(int, input().split())) for _ in range(R)]
-up = 0
-down = 0
-
-for i in range(R):
+# 공기청정기는 항상 1번 열에 설치
+for i in range(N):
     if lst[i][0] == -1:
-        up = i
-        down = i + 1
+        airs.append(i)
+        airs.append(i + 1)
         break
 
-# 미세먼지 확산
-def diffusion():
-    temp = [[0] * C for _ in range(R)]
+directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
 
-    for r in range(R):
-        for c in range(C):
-            if lst[r][c] > -1:
-                n = lst[r][c] // 5
-                cnt = 0
-                for dr, dc in ((0, 1), (1, 0), (0, -1), (-1, 0)):
-                    nr, nc = r + dr, c + dc
-                    if 0 <= nr < R and 0 <= nc < C and lst[nr][nc] != -1:
-                        temp[nr][nc] += n
-                        cnt += 1
-                lst[r][c] -= (lst[r][c] // 5) * cnt
+for _ in range(T):
+    # lst_change[i][j]: (i, j)칸이 먼지 확산 이후에 변해야 하는 양
+    lst_change = [[0] * M for _ in range(N)]
 
-    for r in range(R):
-        for c in range(C):
-            lst[r][c] += temp[r][c]
+    for i in range(N):
+        for j in range(M):
+            if lst[i][j] <= 0:
+                continue
+
+            # 미세먼지는 인접한 네 방향으로 확산
+            for dy, dx in directions:
+                ny, nx = i + dy, j + dx
+
+                # 인접한 방향에 공기청정기가 있거나, 칸이 없으면 그 방향으로는 확산이 일어나지 않는다.
+                if not (0 <= ny < N and 0 <= nx < M) or lst[ny][nx] == -1:
+                    continue
+
+                change = lst[i][j] // 5
+
+                # 확산되는 양은 5를 나눈 값에 소수점은 버린다
+                lst_change[ny][nx] += change
+
+                # 남은 미세먼지의 양은 (5를 나눈 값)×(확산된 방향의 개수)
+                lst_change[i][j] -= change
+
+    # 확산은 미세먼지가 있는 모든 칸에서 동시에 일어난다
+    for i in range(N):
+        for j in range(M):
+            lst[i][j] += lst_change[i][j]
+
+    # 공기청정기가 작동
+    # 공기청정기에서 부는 바람은 미세먼지가 없는 바람이고,
+    # 공기청정기로 들어간 미세먼지는 모두 정화
+
+    # 위쪽 공기청정기의 바람은 반시계방향으로 순환
+    for n in range(airs[0] - 1, 0, -1):
+        lst[n][0] = lst[n - 1][0]
+
+    for m in range(M - 1):
+        lst[0][m] = lst[0][m + 1]
+
+    for n in range(airs[0]):
+        lst[n][-1] = lst[n + 1][-1]
+
+    for m in range(M - 1, 1, -1):
+        lst[airs[0]][m] = lst[airs[0]][m - 1]
+
+    lst[airs[0]][1] = 0
+
+    # 아래쪽 공기청정기의 바람은 시계방향으로 순환
+    for n in range(airs[1] + 1, N - 1):
+        lst[n][0] = lst[n + 1][0]
+
+    for m in range(M - 1):
+        lst[-1][m] = lst[-1][m + 1]
+
+    for n in range(N - 1, airs[1], -1):
+        lst[n][-1] = lst[n - 1][-1]
+
+    for m in range(M - 1, 1, -1):
+        lst[airs[1]][m] = lst[airs[1]][m - 1]
+
+    lst[airs[1]][1] = 0
 
 
-# 공기청정기 작동
-def airpurifier():
-    # up
-    temp1 = lst[up][-1]
-    temp2 = lst[0][-1]
-    temp3 = lst[0][0]
-
-    for c in range(C - 1, 1, -1):
-        lst[up][c] = lst[up][c - 1]
-    lst[up][1] = 0
-
-    for r in range(up - 1):
-        lst[r][-1] = lst[r + 1][-1]
-    lst[up - 1][-1] = temp1
-
-    for c in range(C - 1):
-        lst[0][c] = lst[0][c + 1]
-    lst[0][-2] = temp2
-
-    for r in range(up - 1, 1, -1):
-        lst[r][0] = lst[r - 1][0]
-    lst[1][0] = temp3
-
-
-    # down
-    temp1 = lst[down][-1]
-    temp2 = lst[-1][-1]
-    temp3 = lst[-1][0]
-
-    for c in range(C - 1, 1, -1):
-        lst[down][c] = lst[down][c - 1]
-    lst[down][1] = 0
-
-    for r in range(R - 1, down + 1, -1):
-        lst[r][-1] = lst[r - 1][-1]
-    lst[down + 1][-1] = temp1
-
-    for c in range(C - 1):
-        lst[-1][c] = lst[-1][c + 1]
-    lst[-1][-2] = temp2
-
-    for r in range(down + 1, R - 1):
-        lst[r][0] = lst[r + 1][0]
-    lst[-2][0] = temp3
-
-
-for t in range(T):
-    diffusion()
-    airpurifier()
-
+# T초가 지난 후 구사과의 방에 남아있는 미세먼지의 양
 result = 0
-
 for line in lst:
-    for item in line:
-        if item != -1:
-            result += item
+    result += sum(line)
 
-print(result)
+# air 2개가 -1이라서 2 더함
+print(result + 2)
