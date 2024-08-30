8f5fd5aae0b842064e7b325ce872536f78892c76
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-08-30 10:05:17 +0900

    [Gold III] Title: 연구소 3, Time: 184 ms, Memory: 114848 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.py
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -1,70 +1,76 @@
 from collections import deque
 
-N, M = map(int, input().split())
+N, H = map(int, input().split())
+# 크기가 N×N
+M = N
+# 0은 빈 칸, 1은 벽, 2는 비활성 바이러스
 lst = [list(map(int, input().split())) for _ in range(N)]
-viruses = []
 
+not_activated = []
+blank_cnt = 0    # 병원과 벽을 제외한 모든 지역에 바이러스
 for i in range(N):
-    for j in range(N):
+    for j in range(M):
         if lst[i][j] == 2:
-            viruses.append((i, j))
+            not_activated.append((i, j))
+        elif lst[i][j] == 0:
+            blank_cnt += 1
+length = len(not_activated)
 
-length = len(viruses)
-used = [0] * length
-result = 21e8
+directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
 
-def check():
-    q = deque()
-    visited = [[0] * N for _ in range(N)]
-
-    for i in range(length):
-        if used[i]:
-            q.append((*viruses[i], 0))
-            visited[viruses[i][0]][viruses[i][1]] = 1
-    
-    time = 0
-
-    while q:
-        nowy, nowx, nowc = q.popleft()
-    
-        for dy, dx in ((0, 1), (1, 0), (0, -1), (-1, 0)):
-            ny, nx = nowy + dy, nowx + dx
-            if 0 <= ny < N and 0 <= nx < N and not visited[ny][nx]:
-                if lst[ny][nx] == 0:
-                    visited[ny][nx] = 1
-                    q.append((ny, nx, nowc + 1))
-                    time = max(time, nowc + 1)
-                elif lst[ny][nx] == 2:
-                    visited[ny][nx] = 1
-                    q.append((ny, nx, nowc + 1))
-    
-    if time > result:
-        return 21e8
-
-    for i in range(N):
-        for j in range(N):
-            if not visited[i][j] and lst[i][j] == 0:
-                return 21e8
-    
-    return time
-            
-
-def compose(level, start):
+# H가 최대 10이므로 dfs로 조합 만들어서 bfs 돌리면 되겠다
+selected = [0] * length
+# 활성 바이러스가 하나만 있을 때 전체에 퍼지기 위해 필요한 가장 긴 시간(대충)
+MAX = N * M
+# 모든 빈 칸에 바이러스를 퍼뜨리는 최소 시간
+result = MAX
+def dfs(level, start):
     global result
 
-    if level == M:
-        result = min(result, check())
+    # H개의 비활성 바이러스를 적절히 골라
+    if level == H:
+        # bfs
+        # 활성 상태인 바이러스는 상하좌우로 인접한 모든 빈 칸으로 동시에 복제
+        q = deque()
+        used = [[0] * M for _ in range(N)]
+        left_blank_cnt = blank_cnt
+
+        for i in range(length):
+            if selected[i]:
+                q.append(not_activated[i])
+                used[not_activated[i][0]][not_activated[i][1]] = 1
+
+        time = 0
+        while left_blank_cnt and q:
+            time += 1
+            for _ in range(len(q)):
+                y, x = q.popleft()
+                for dy, dx in directions:
+                    ny, nx = y + dy, x + dx
+                    if not (0 <= ny < N and 0 <= nx < M) or lst[ny][nx] == 1:
+                        continue
+
+                    if used[ny][nx]:
+                        continue
+
+                    used[ny][nx] = 1
+                    q.append((ny, nx))
+
+                    if lst[ny][nx] == 0:
+                        left_blank_cnt -= 1
+
+        if not left_blank_cnt:
+            result = min(result, time)
+
         return
-    
+
     for i in range(start, length):
-        if not used[i]:
-            used[i] = 1
-            compose(level + 1, i + 1)
-            used[i] = 0
+        if not selected[i]:
+            selected[i] = 1
+            dfs(level + 1, i + 1)
+            selected[i] = 0
 
-compose(0, 0)
+dfs(0, 0)
 
-if result != 21e8:
-    print(result)
-else:
-    print(-1)
+# 바이러스를 어떻게 놓아도 모든 빈 칸에 바이러스를 퍼뜨릴 수 없는 경우에는 -1
+print(result if result != MAX else -1)
