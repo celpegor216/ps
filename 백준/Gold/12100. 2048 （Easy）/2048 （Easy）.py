31ec035f54f07f645a997fa09978f720886db65a
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-02 17:08:16 +0900

    [Gold II] Title: 2048 (Easy), Time: 200 ms, Memory: 112684 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -18,9 +18,12 @@ def dfs(level, lst):
     global result
 
     # 5번 움직인 이후에 격자판에서 가장 큰 값의 최댓값을 구하는 프로그램
+    # 한 번 움직인 이후로 같은 방향으로 계속 움직이면 어차피 그대로니까
+    # 매번 판이 바뀔 때마다 최댓값 계산
+    for line in lst:
+        result = max(result, max(line))
+
     if level == L:
-        for line in lst:
-            result = max(result, max(line))
         return
 
     for i in range(4):
@@ -31,6 +34,8 @@ def dfs(level, lst):
         # 중력에 의해 부딪히게 될 벽(바닥)에서 가까운 숫자부터 두 개씩만 합쳐집니다
         # 단 한 번의 중력작용으로 이미 합쳐진 숫자가 연쇄적으로 합쳐지진 않습니다
 
+        # 움직였는데 이전과 동일하다면 dfs 타고 들어가지 않음
+        flag = 0
         for m in range(M):
             last_merged = N + 1
             for n in range(N - 2, -1, -1):
@@ -42,12 +47,17 @@ def dfs(level, lst):
                                 nxt[y][m] = 0
                                 nxt[y + 1][m] *= 2
                                 last_merged = y + 1
+                                flag = 1
                             break
                         else:
                             nxt[y][m], nxt[y + 1][m] = nxt[y + 1][m], nxt[y][m]
+                            flag = 1
                             y += 1
 
-        dfs(level + 1, rotate(4 - i, nxt))
+        nxt = rotate(4 - i, nxt)
+
+        if flag:
+            dfs(level + 1, nxt)
 
 
 dfs(0, [list(map(int, input().split())) for _ in range(N)])
