4fbd5fa19e943522d9c9bca2a0071329c0a56aa4
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-03 19:27:46 +0900

    [Gold IV] Title: 불, Time: 660 ms, Memory: 235192 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.py
+++ 백준/Gold/5427. 불/불.py
@@ -1,51 +1,46 @@
 from collections import deque
 
-T = int(input())
+TC = int(input())
 
-for t in range(T):
+for _ in range(TC):
     M, N = map(int, input().split())
-    lst = [input() for _ in range(N)]
+    lst = [list(input()) for _ in range(N)]
 
-    sy = sx = -1
+    person = []
     fire = []
+    used = [[0] * M for _ in range(N)]
+    for i in range(N):
+        for j in range(M):
+            if lst[i][j] == '@':
+                person.append((i, j, 2, 0))
+                lst[i][j] = '.'
+                used[i][j] = 2
+            elif lst[i][j] == '*':
+                fire.append((i, j, 1, 0))
+                lst[i][j] = '.'
+                used[i][j] = 1
 
-    for n in range(N):
-        for m in range(M):
-            if lst[n][m] == '@':
-                sy = n
-                sx = m
-            elif lst[n][m] == '*':
-                fire.append((n, m))
-    
-    used = [[[0, 0] for _ in range(M)] for _ in range(N)]
+    # 벽을 통과할 수 없고, 불이 옮겨진 칸 또는 이제 불이 붙으려는 칸으로 이동할 수 없다
+    # 상근이가 있는 칸에 불이 옮겨옴과 동시에 다른 칸으로 이동할 수 있다
 
-    q = deque()
-    for y, x in fire:
-        q.append((y, x, 0, 0))
-        used[y][x][0] = 1
-    q.append((sy, sx, 1, 0))
-    used[sy][sx][1] = 1
+    # 불이 먼저 움직인 다음에 상근이가 이동
+    q = deque(fire + person)
 
-    result = -1
+    result = 'IMPOSSIBLE'
+    while q:
+        y, x, t, cnt = q.popleft()
 
-    while q and result == -1:
-        nowy, nowx, nowt, nowc = q.popleft()
+        if t == 2 and (y in (0, N - 1) or x in (0, M - 1)):
+            result = cnt + 1
+            break
 
         for dy, dx in ((0, 1), (1, 0), (0, -1), (-1, 0)):
-            ny, nx = nowy + dy, nowx + dx
-            if nowt == 0:
-                if 0 <= ny < N and 0 <= nx < M and not used[ny][nx][nowt] and lst[ny][nx] != '#':
-                    used[ny][nx][nowt] = 1
-                    q.append((ny, nx, nowt, nowc + 1))
-            else:
-                if 0 <= ny < N and 0 <= nx < M and not sum(used[ny][nx]) and lst[ny][nx] != '#':
-                    used[ny][nx][nowt] = 1
-                    q.append((ny, nx, nowt, nowc + 1))
-                elif not (0 <= ny < N and 0 <= nx < M):
-                    result = nowc + 1
-                    break
-    
-    if result == -1:
-        print("IMPOSSIBLE")
-    else:
-        print(result)
+            ny, nx = y + dy, x + dx
+            if 0 <= ny < N and 0 <= nx < M and lst[ny][nx] == '.':
+                # 사람은 자기가 있었던 곳이나 불이 있었던 곳에 갈 수 없음
+                # 불은 사람이 있었던 곳에는 갈 수 있음
+                if (t == 2 and not used[ny][nx]) or (t == 1 and used[ny][nx] != 1):
+                    used[ny][nx] = t
+                    q.append((ny, nx, t, cnt + 1))
+
+    print(result)
