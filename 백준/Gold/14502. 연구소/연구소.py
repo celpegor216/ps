ca52bbfe9b59341bbe67a1af7b3818618e39df50
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-06 08:54:05 +0900

    [Gold IV] Title: 연구소, Time: 364 ms, Memory: 115684 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,62 +1,56 @@
-from copy import deepcopy
 from collections import deque
 
 N, M = map(int, input().split())
-
+# 0은 빈 칸, 1은 벽, 2는 바이러스가 있는 곳
 lst = [list(map(int, input().split())) for _ in range(N)]
-
-empty = []
+blanks = []
 virus = []
+for i in range(N):
+    for j in range(M):
+        if lst[i][j] == 0:
+            blanks.append((i, j))
+        elif lst[i][j] == 2:
+            virus.append((i, j))
 
-for n in range(N):
-    for m in range(M):
-        if lst[n][m] == 0:
-            empty.append((n, m))
-        elif lst[n][m] == 2:
-            virus.append((n, m))
+MAX = len(blanks)
 
-length = len(empty)
-used = [0] * length
-
-def dfs(level, now):
-    if level == 3:
-        bfs()
-        return
-
-    for i in range(now + 1, length):
-        used[i] = 1
-        lst[empty[i][0]][empty[i][1]] = 1
-        dfs(level + 1, i)
-        used[i] = 0
-        lst[empty[i][0]][empty[i][1]] = 0
-
+# 새로 세울 수 있는 벽의 개수는 3개이며, 꼭 3개를 세워야 한다
 result = 0
-
-def bfs():
+def dfs(level, start):
     global result
 
-    q = deque(virus)
-    visited = deepcopy(lst)
+    # 벽을 3개 세운 뒤, 바이러스가 퍼질 수 없는 곳을 안전 영역
+    if level == 3:
+        # bfs
+        q = deque(virus)
+        used = [[0] * M for _ in range(N)]
 
-    while q:
-        nowy, nowx = q.popleft()
+        while q:
+            y, x = q.popleft()
 
-        visited[nowy][nowx] = 2
+            for dy, dx in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+                ny, nx = y + dy, x + dx
+                if 0 <= ny < N and 0 <= nx < M and not used[ny][nx] and not lst[ny][nx] :
+                    used[ny][nx] = 1
+                    q.append((ny, nx))
 
-        for dy, dx in ((0, 1), (1, 0), (0, -1), (-1, 0)):
-            ny, nx = nowy + dy, nowx + dx
-            if 0 <= ny < N and 0 <= nx < M and visited[ny][nx] == 0:
-                q.append((ny, nx))
+        total = 0
+        for i in range(N):
+            for j in range(M):
+                if not used[i][j] and not lst[i][j]:
+                    total += 1
 
-    cnt = 0
-    for n in range(N):
-        for m in range(M):
-            if visited[n][m] == 0:
-                cnt += 1
+        result = max(result, total)
+        return
 
-    if cnt > result:
-        result = cnt
+    for idx in range(start, MAX):
+        y, x = blanks[idx]
+        if not lst[y][x]:
+            lst[y][x] = 1
+            dfs(level + 1, idx + 1)
+            lst[y][x] = 0
 
-dfs(0, -1)
+dfs(0, 0)
 
+# 얻을 수 있는 안전 영역 크기의 최댓값
 print(result)
