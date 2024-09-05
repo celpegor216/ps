5b866eb29460e318736f8ced22c263a57d4f0b8c
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-05 19:40:07 +0900

    [Gold III] Title: 로봇, Time: 144 ms, Memory: 115336 KB -BaekjoonHub

--- 백준/Gold/1726. 로봇/로봇.py
+++ 백준/Gold/1726. 로봇/로봇.py
@@ -16,14 +16,11 @@ turn_right = (2, 3, 1, 0)
 
 q = deque()
 q.append((0, sy, sx, sd))
-used = [[[21e8] * 4 for _ in range(M)] for _ in range(N)]
-used[sy][sx][sd] = 0
+used = [[[0] * 4 for _ in range(M)] for _ in range(N)]
+used[sy][sx][sd] = 1
 
 while q:
     cnt, y, x, d = q.popleft()
-
-    if used[y][x][d] < cnt:
-        continue
 
     # 최소 몇 번의 명령이 필요한지 구하는 프로그램
     # 출발지점에서 도착지점까지는 항상 이동이 가능
@@ -34,18 +31,16 @@ while q:
     # 명령 1. Go k: k는 1, 2 또는 3일 수 있다. 현재 향하고 있는 방향으로 k칸 만큼 움직인다.
     for k in range(1, 4):
         ny, nx = y + directions[d][0] * k, x + directions[d][1] * k
-        if not(0 <= ny < N and 0 <= nx < M):
+        if not(0 <= ny < N and 0 <= nx < M) or lst[ny][nx]:
             break
-        if lst[ny][nx]:
-            break
-        if used[ny][nx][d] > cnt + 1:
-            used[ny][nx][d] = cnt + 1
+        if not used[ny][nx][d]:
+            used[ny][nx][d] = 1
             q.append((cnt + 1, ny, nx, d))
 
     # 명령 2. Turn dir: dir은 left 또는 right 이며, 각각 왼쪽 또는 오른쪽으로 90° 회전한다.
-    if used[y][x][turn_left[d]] > cnt + 1:
-        used[y][x][turn_left[d]] = cnt + 1
+    if not used[y][x][turn_left[d]]:
+        used[y][x][turn_left[d]] = 1
         q.append((cnt + 1, y, x, turn_left[d]))
-    if used[y][x][turn_right[d]] > cnt + 1:
-        used[y][x][turn_right[d]] = cnt + 1
+    if not used[y][x][turn_right[d]]:
+        used[y][x][turn_right[d]] = 1
         q.append((cnt + 1, y, x, turn_right[d]))
