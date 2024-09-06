f4fa32d4fd4f56edf9119a6ddf25afefb45cde52
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-06 12:39:18 +0900

    [Gold IV] Title: 뱀, Time: 116 ms, Memory: 113944 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,48 +1,54 @@
 from collections import deque
 
+
+# NxN
 N = int(input())
+M = N
+# 사과의 개수
 K = int(input())
-apples = [list(map(int, input().split())) for _ in range(K)]
-
-L = int(input())
-moves = dict()
-
-for l in range(L):
-    time, command = input().split()
-    moves[int(time)] = command
-
-directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-length = 1
-snakes = deque()
-snakes.append([1, 1])
+# 사과의 위치, 1 ~ N
+apples = [[0] * (M + 1) for _ in range(N + 1)]
+for _ in range(K):
+    i, j = map(int, input().split())
+    apples[i][j] = 1
+# 게임이 시작할때 뱀은 맨위 맨좌측에 위치하고 뱀의 길이는 1 이다. 뱀은 처음에 오른쪽을 향한다.
+snake = deque()
+snake.append((1, 1))
 d = 0
 
-cnt = 0
+directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
+L = int(input())
+moves = []
+for _ in range(L):
+    x, c = input().split()
+    moves.append((int(x), 1 if c == 'D' else -1))
+move_idx = 0
+
+result = 0
 while 1:
-    y, x = snakes[-1]
+    result += 1
 
-    if moves.get(cnt):
-        if moves[cnt] == 'D':
-            d += 1
-            if d > 3:
-                d = 0
-        else:
-            d -= 1
-            if d < 0:
-                d = 3
+    # 먼저 뱀은 몸길이를 늘려 머리를 다음칸에 위치시킨다.
+    hy, hx = snake[0]
+    ny, nx = hy + directions[d][0], hx + directions[d][1]
 
-    ny, nx = y + directions[d][0], x + directions[d][1]
-    
-    if not (0 < ny <= N and 0 < nx <= N) or [ny, nx] in snakes:
+    # 만약 벽이나 자기자신의 몸과 부딪히면 게임이 끝난다.
+    # 보드의 상하좌우 끝에 벽
+    if not(1 <= ny <= N and 1 <= nx <= M) or (ny, nx) in snake:
+        print(result)
         break
 
-    snakes.append([ny, nx])
-    if [ny, nx] in apples:
-        apples.remove([ny, nx])
+    snake.appendleft((ny, nx))
+    # 만약 이동한 칸에 사과가 있다면, 그 칸에 있던 사과가 없어지고 꼬리는 움직이지 않는다.
+    # 만약 이동한 칸에 사과가 없다면, 몸길이를 줄여서 꼬리가 위치한 칸을 비워준다.
+    # 즉, 몸길이는 변하지 않는다.
+    if apples[ny][nx]:
+        apples[ny][nx] = 0
     else:
-        snakes.popleft()
-    
-    cnt += 1
+        snake.pop()
 
-print(cnt + 1)
+    # 게임 시작 시간으로부터 X초가 끝난 뒤에 왼쪽(C가 'L') 또는 오른쪽(C가 'D')로 90도 방향을 회전
+    if move_idx < L and moves[move_idx][0] == result:
+        d = (d + moves[move_idx][1]) % 4
+        move_idx += 1
