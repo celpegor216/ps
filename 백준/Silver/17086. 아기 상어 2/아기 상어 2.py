36c7d2ae752eb276c07a3166611e3a35a6c90cf0
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-07 09:32:13 +0900

    [Silver II] Title: 아기 상어 2, Time: 132 ms, Memory: 114028 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
@@ -3,27 +3,23 @@ from collections import deque
 N, M = map(int, input().split())
 lst = [list(map(int, input().split())) for _ in range(N)]
 
-used = [[-1] * M for _ in range(N)]
 q = deque()
+used = [[21e8] * M for _ in range(N)]
+for i in range(N):
+    for j in range(M):
+        if lst[i][j]:
+            q.append((i, j, 0))
+            used[i][j] = 0
 
-for n in range(N):
-    for m in range(M):
-        if lst[n][m] == 1:
-            used[n][m] = 0
-            q.append((n, m, 0))
+directions = ((0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1))
 
 while q:
-    nowy, nowx, nowc = q.popleft()
+    y, x, cnt = q.popleft()
 
-    for dy, dx in ((0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)):
-        ny, nx = nowy + dy, nowx + dx
-        if 0 <= ny < N and 0 <= nx < M and used[ny][nx] == -1:
-            used[ny][nx] = nowc + 1
-            q.append((ny, nx, nowc + 1))
+    for dy, dx in directions:
+        ny, nx = y + dy, x + dx
+        if 0 <= ny < N and 0 <= nx < M and used[ny][nx] > cnt + 1:
+            used[ny][nx] = cnt + 1
+            q.append((ny, nx, cnt + 1))
 
-result = 0
-for n in range(N):
-    for m in range(M):
-        result = max(used[n][m], result)
-
-print(result)
+print(max([max(line) for line in used]))
