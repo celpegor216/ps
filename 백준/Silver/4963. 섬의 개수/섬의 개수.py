588323055f140ca90e3794c4f7dc92799fc158b9
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-07 09:21:44 +0900

    [Silver II] Title: 섬의 개수, Time: 160 ms, Memory: 115652 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,32 +1,36 @@
 from collections import deque
 
+directions = ((0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1))
+
 while 1:
     M, N = map(int, input().split())
 
     if N == M == 0:
         break
-
+    
     lst = [list(map(int, input().split())) for _ in range(N)]
-    used = [[0] * M for _ in range(N)]
+
     result = 0
+    used = [[0] * M for _ in range(N)]
 
-    for n in range(N):
-        for m in range(M):
-            if lst[n][m] and not used[n][m]:
-                q = deque()
-                q.append((n, m))
+    for i in range(N):
+        for j in range(M):
+            if not lst[i][j] or used[i][j]:
+                continue
 
-                used[n][m] = 1
+            result += 1
+            used[i][j] = 1
 
-                while q:
-                    y, x = q.popleft()
-
-                    for dy, dx in ((0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)):
-                        ny, nx = y + dy, x + dx
-                        if 0 <= ny < N and 0 <= nx < M and not used[ny][nx] and lst[ny][nx]:
-                            q.append((ny, nx))
-                            used[ny][nx] = 1
+            q = deque()
+            q.append((i, j))
+            
+            while q:
+                y, x = q.popleft()
                 
-                result += 1
+                for dy, dx in directions:
+                    ny, nx = y + dy, x + dx
+                    if 0 <= ny < N and 0 <= nx < M and lst[ny][nx] and not used[ny][nx]:
+                        used[ny][nx] = 1
+                        q.append((ny, nx))
     
     print(result)
