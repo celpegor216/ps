6fe4843f9f50c20fd8f282330876e39b6b04006a
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-07 16:21:04 +0900

    [Silver II] Title: 배열 돌리기, Time: 392 ms, Memory: 135868 KB -BaekjoonHub

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
@@ -1,48 +1,44 @@
 T = int(input())
 
-def turn_anticlockwise():
-    center = N // 2
+# 시계 방향으로 45도 회전
+def rotate():
+    global lst
 
-    for i in range(1, center + 1):
-        temp = lst[center - i][center - i]
+    new_lst = [line[:] for line in lst]
 
-        lst[center - i][center - i] = lst[center - i][center]
-        lst[center - i][center] = lst[center - i][center + i]
-        lst[center - i][center + i] = lst[center][center + i]
-        lst[center][center + i] = lst[center + i][center + i]
-        lst[center + i][center + i] = lst[center + i][center]
-        lst[center + i][center] = lst[center + i][center - i]
-        lst[center + i][center - i] = lst[center][center - i]
-        lst[center][center - i] = temp
+    # X의 주 대각선을 ((1,1), (2,2), …, (n, n)) 가운데 열 ((n+1)/2 번째 열)로 옮긴다.
+    for i in range(N):
+        new_lst[i][N // 2] = lst[i][i]
 
+    # X의 가운데 열을 X의 부 대각선으로 ((n, 1), (n-1, 2), …, (1, n)) 옮긴다. 
+    for i in range(N):
+        new_lst[i][N - i - 1] = lst[i][N // 2]
 
-def turn_clockwise():
-    center = N // 2
+    # X의 부 대각선을 X의 가운데 행 ((n+1)/2번째 행)으로 옮긴다.
+    for i in range(N):
+        new_lst[N // 2][N - i - 1] = lst[i][N - i - 1]
 
-    for i in range(1, center + 1):
-        temp = lst[center + i][center + i]
+    # X의 가운데 행을 X의 주 대각선으로 옮긴다.
+    for i in range(N):
+        new_lst[i][i] = lst[N // 2][i]
 
-        lst[center + i][center + i] = lst[center][center + i]
-        lst[center][center + i] = lst[center - i][center + i]
-        lst[center - i][center + i] = lst[center - i][center]
-        lst[center - i][center] = lst[center - i][center - i]
-        lst[center - i][center - i] = lst[center][center - i]
-        lst[center][center - i] = lst[center + i][center - i]
-        lst[center + i][center - i] = lst[center + i][center]
-        lst[center + i][center] = temp
+    # 위 네 가지 경우 모두 원소의 기존 순서는 유지 되어야 한다.
+    # X의 다른 원소의 위치는 변하지 않는다.
 
+    lst = new_lst
+    
 
-for t in range(T):
+for _ in range(T):
     N, D = map(int, input().split())
-
     lst = [list(map(int, input().split())) for _ in range(N)]
 
-    if D < 0:
-        for d in range(-D // 45):
-            turn_anticlockwise()
-    else:
-        for d in range(D // 45):
-            turn_clockwise()
-
+    if D != 0:
+        if D > 0:
+            for _ in range(D // 45):
+                rotate()
+        else:
+            for _ in range((360 + D) // 45):
+                rotate()
+    
     for line in lst:
         print(*line)
