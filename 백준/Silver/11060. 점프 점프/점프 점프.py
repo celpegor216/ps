1fe4ad7093d601c8fa1acc27eac93706125bebd2
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-07 09:26:21 +0900

    [Silver II] Title: 점프 점프, Time: 108 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.py
+++ 백준/Silver/11060. 점프 점프/점프 점프.py
@@ -3,30 +3,28 @@ from collections import deque
 N = int(input())
 lst = list(map(int, input().split()))
 
-used = [21e8] * N
-used[0] = 0
-
 q = deque()
 q.append((0, 0))
 
+used = [0] * N
+used[0] = 1
+
+result = -1
 while q:
     now, cnt = q.popleft()
 
-    if used[now] < cnt:
-        continue
-
     if now == N - 1:
+        result = cnt
         break
 
     for i in range(1, lst[now] + 1):
-        if now + i >= N:
+        nxt = now + i
+        
+        if nxt >= N:
             break
 
-        if used[now + i] > cnt + 1:
-            used[now + i] = cnt + 1
-            q.append((now + i, cnt + 1))
+        if not used[nxt]:
+            used[nxt] = 1
+            q.append((nxt, cnt + 1))
 
-if used[-1] == 21e8:
-    print(-1)
-else:
-    print(used[-1])
+print(result)
