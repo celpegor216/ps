74ebcc978f9cfea97befa65e785cd8410a9fcb03
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-07 13:59:32 +0900

    [Bronze II] Title: 거리의 합, Time: 420 ms, Memory: 110708 KB -BaekjoonHub

--- 백준/Bronze/2399. 거리의 합/거리의 합.py
+++ 백준/Bronze/2399. 거리의 합/거리의 합.py
@@ -4,7 +4,6 @@ lst = list(map(int, input().split()))
 result = 0
 for i in range(N - 1):
     for j in range(i + 1, N):
-        tmp = (lst[i] - lst[j]) * 2
-        result += tmp if tmp > 0 else -tmp
+        result += abs(lst[i] - lst[j]) * 2
 
 print(result)
