c080a866fa11c04842f6c98ae91d6fbd57e895ab
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-10 19:46:31 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -1,29 +1,26 @@
-# 시간초과 해결 방법: array[N-1][N-1]이 1인지 검사하는 코드를 추가
-
 N = int(input())
-lst = [list(map(int, input().split())) for _ in range(N)]
+# 빈 칸은 0, 벽은 1
+lst = [[1] * (N + 1)] + [[1] + list(map(int, input().split())) for _ in range(N)]
 
-result = 0
+# dp[i][j][k]: i, j에 가로/세로/대각선으로 도착할 수 있는 경우의 수
+dp = [[[0] * 3 for _ in range(N + 1)] for _ in range(N + 1)]
 
-types = {0: [0, 2], 1: [1, 2], 2: [0, 1, 2]}
+# 가장 처음에 파이프는 (1, 1)와 (1, 2)를 차지하고 있고, 방향은 가로
+dp[1][2][0] = 1
 
-def dfs(y, x, nowt):
-    global result
-    
-    if y == N - 1 and x == N - 1:
-        result += 1
-        return
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        if lst[i][j]:
+            continue
 
-    for t in types[nowt]:
-        if t == 0 and x + 1 < N and lst[y][x + 1] != 1:
-            dfs(y, x + 1, t)
-        elif t == 1 and y + 1 < N and lst[y + 1][x] != 1:
-            dfs(y + 1, x, t)
-        elif t == 2 and x + 1 < N and y + 1 < N and lst[y][x + 1] != 1 and lst[y + 1][x] != 1 and lst[y + 1][x + 1] != 1:
-            dfs(y + 1, x + 1, t)
+        # 가로로 도착할 수 있으려면 이전에 가로/대각선이었어야 함
+        dp[i][j][0] += dp[i][j - 1][0] + dp[i][j - 1][2]
 
-if lst[-1][-1] == 1:
-    print(0)
-else:
-    dfs(0, 1, 0)
-    print(result)
+        # 세로로 도착할 수 있으려면 이전에 세로/대각선이었어야 함
+        dp[i][j][1] += dp[i - 1][j][1] + dp[i - 1][j][2]
+
+        # 대각선으로 도착할 수 있으려면 이전에 가로/세로/대각선이었어야 함
+        if not (lst[i][j - 1] or lst[i - 1][j]):
+             dp[i][j][2] += sum(dp[i - 1][j - 1])
+
+print(sum(dp[-1][-1]))
