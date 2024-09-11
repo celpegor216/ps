26a5c05fdc551b450468249a6659d0c14c2f04d7
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-11 17:44:52 +0900

    [Gold I] Title: 어른 상어, Time: 148 ms, Memory: 111844 KB -BaekjoonHub

--- 백준/Gold/19237. 어른 상어/어른 상어.py
+++ 백준/Gold/19237. 어른 상어/어른 상어.py
@@ -1,71 +1,95 @@
-import sys
-input = sys.stdin.readline
+'''
+와...................................................
+예전에 풀었던 코드 봤는데 오늘 내가 문제를 완전 잘못 이해하고 있었네,,,,,,,,,
+한 칸에 남아있을 수 있는 냄새가 여러 개라고 생각해서 크기 K인 deque를 썼었는데
+어차피 다른 냄새가 있는 칸으로는 이동하지 않으니까 한 칸에는 하나의 냄새만 남아있어서
+굳이 크기가 K일 필요가 없었네,,,,,,, 이거 시간초과 각이었구나,,,,,,,,ㅋ,,,,,,,,,,,
+진짜 운 좋았네,,,,,,,,,,,,,,,,,
+'''
 
+
+
+
+
+# N×N 크기의 격자 중 M개의 칸에 상어가 한 마리씩 들어 있다
 N, M, K = map(int, input().split())
-lst = [list(map(int, input().split())) for _ in range(N)]
-
-sharks = dict()
-used = [[[0, 0] for _ in range(N)] for _ in range(N)]
-
+# 상어에는 1 이상 M 이하의 자연수 번호, 모든 번호는 서로 다르다
+dic = dict()
 for i in range(N):
+    tmp = list(map(int, input().split()))
     for j in range(N):
-        if lst[i][j]:
-            sharks[lst[i][j]] = [lst[i][j], i, j]
-            used[i][j] = [lst[i][j], K]
+        if not tmp[j]:
+            continue
 
-tmp = list(map(int, input().split()))
-for m in range(M):
-    sharks[m + 1].append(tmp[m])
+        dic[(i, j)] = tmp[j] - 1
 
-directions = [(), (-1, 0), (1, 0), (0, -1), (0, 1)]
-sharks_directions = [[]] + [[[]] + [list(map(int, input().split())) for _ in range(4)] for _ in range(M)]
+D = 4
+directions = ((-1, 0), (1, 0), (0, -1), (0, 1))
 
-result = 0
+def transform(x):
+    return int(x) - 1
 
-while len(sharks.keys()) > 1 and result <= 1000:
-    # 번호가 작은 상어부터 이동
-    tmp_sharks = dict()
-    tmp_used = [[[] for _ in range(N)] for _ in range(N)]
+# 상어가 맨 처음에 보고 있는 방향은 입력으로 주어지고,
+# 그 후에는 방금 이동한 방향이 보고 있는 방향이 된다
+shark_directions_now = list(map(transform, input().split()))
 
-    for key in sorted(sharks.keys()):
-        nown, nowy, nowx, nowd = sharks[key]
+# 상어마다, 방향마다 우선순위
+shark_directions_priority = [[list(map(transform, input().split())) for _ in range(D)] for _ in range(M)]
 
-        flag = 0
-        mine = []
+smells = [[[0] * 2 for _ in range(N)] for _ in range(N)]
 
-        for d in sharks_directions[nown][nowd]:
-            ny, nx = nowy + directions[d][0], nowx + directions[d][1]
+for result in range(1, 1001):
+    # 맨 처음에는 모든 상어가 자신의 위치에 자신의 냄새를 뿌린다
+    # 1초마다 모든 상어가 동시에 상하좌우로 인접한 칸 중 하나로 이동하고,
+    # 자신의 냄새를 그 칸에 뿌린다
+    for pos, idx in dic.items():
+        y, x = pos
+        smells[y][x] = [idx, K]
 
-            if 0 <= ny < N and 0 <= nx < N:
-                if not used[ny][nx][0]:
-                    tmp_used[ny][nx].append([nown, d])
-                    flag = 1
-                    break
-                elif used[ny][nx][0] == nown and not mine:
-                    mine = [ny, nx, d]
-            
-        if not flag and mine:
-            tmp_used[mine[0]][mine[1]].append([nown, mine[2]])
-    
-    # 1초마다 냄새가 옅어짐
+    # 각 상어가 이동 방향을 결정할 때는,
+    new_dic = dict()
+    for pos, idx in dic.items():
+        y, x = pos
+        d = shark_directions_now[idx]
+
+        nd = -1
+        for nxtd in shark_directions_priority[idx][d]:
+            dy, dx = directions[nxtd]
+            ny, nx = y + dy, x + dx
+
+            if not (0 <= ny < N and 0 <= nx < N):
+                continue
+
+            # 먼저 인접한 칸 중 아무 냄새가 없는 칸의 방향으로 잡는다
+            if not smells[ny][nx][-1]:
+                nd = nxtd
+                break
+            # 그런 칸이 없으면 자신의 냄새가 있는 칸의 방향으로 잡는다
+            # 가능한 칸이 여러 개일 수 있는데, 그 경우에는 특정한 우선순위를 따른다.
+            # 우선순위는 상어마다 다를 수 있고,
+            # 같은 상어라도 현재 상어가 보고 있는 방향에 따라 또 다를 수 있다
+            elif nd == -1 and idx == smells[ny][nx][0]:
+                nd = nxtd
+
+        shark_directions_now[idx] = nd
+        ny, nx = y + directions[nd][0], x + directions[nd][1]
+
+        # 모든 상어가 이동한 후 한 칸에 여러 마리의 상어가 남아 있으면,
+        # 가장 작은 번호를 가진 상어를 제외하고 모두 격자 밖으로 쫓겨난다
+        new_dic[(ny, nx)] = min(new_dic.get((ny, nx), M + 1), idx)
+
+    dic = new_dic
+
+    # 냄새는 상어가 k번 이동하고 나면 사라진다.
     for i in range(N):
         for j in range(N):
-            if used[i][j][1]:
-                used[i][j][1] -= 1
+            if smells[i][j][-1]:
+                smells[i][j][-1] -= 1
 
-                if not used[i][j][1]:
-                    used[i][j][0] = 0
-    
-    for i in range(N):
-        for j in range(N):
-            if tmp_used[i][j]:
-                tmp_used[i][j].sort(key=lambda x: x[0])
-
-                n, d = tmp_used[i][j][0]
-                used[i][j] = [n, K]
-                tmp_sharks[n] = [n, i, j, d]
-    
-    sharks = tmp_sharks
-    result += 1
-
-print(result if result <= 1000 else -1)
+    # 1번 상어만 격자에 남게 되기까지 몇 초가 걸리는지를 구하는 프로그램
+    # 1,000초가 넘어도 다른 상어가 격자에 남아 있으면 -1을 출력
+    if len(dic) == 1:
+        print(result)
+        break
+else:
+    print(-1)
