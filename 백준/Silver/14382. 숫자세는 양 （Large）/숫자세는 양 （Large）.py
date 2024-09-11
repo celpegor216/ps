50b727cb55c8db1940afa40f533aae7c81363e5c
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-11 22:55:17 +0900

    [Silver IV] Title: 숫자세는 양 (Large), Time: 104 ms, Memory: 110404 KB -BaekjoonHub

---
+++ 백준/Silver/14382. 숫자세는 양 （Large）/숫자세는 양 （Large）.py
@@ -0,0 +1,23 @@
+TC = int(input())
+
+for tc in range(1, TC + 1):
+    N = input()
+
+    if N == '0':
+        result = 'INSOMNIA'
+    else:
+        used = [0] * 10
+        now = N
+
+        while 1:
+            for n in now:
+                used[int(n)] = 1
+                
+            if sum(used) == 10:
+                break
+
+            now = str(int(now) + int(N))
+
+        result = now
+
+    print(f'Case #{tc}: {result}')
