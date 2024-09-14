4824086d354c06440dcd2f95efc2329d05204e23
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-14 09:34:30 +0900

    [Silver I] Title: 에너지 모으기, Time: 144 ms, Memory: 111796 KB -BaekjoonHub

--- 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
@@ -3,16 +3,18 @@ lst = list(map(int, input().split()))
 
 result = 0
 
-def dfs(total, lst):
+def dfs(level, left, total):
     global result
 
-    if len(lst) < 3:
+    if level == N - 2:
         result = max(result, total)
         return
+    
+    for i in range(1, N - level - 1):
+        new_left = [left[j] for j in range(N - level) if j != i]
+        new_total = total + left[i - 1] * left[i + 1]
+        dfs(level + 1, new_left, new_total)
 
-    for i in range(1, len(lst) - 1):
-        dfs(total + lst[i - 1] * lst[i + 1], lst[:i] + lst[i + 1:])
-
-dfs(0, lst)
+dfs(0, lst, 0)
 
 print(result)
