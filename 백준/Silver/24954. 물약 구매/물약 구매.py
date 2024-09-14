941d5fa4dc5114336cc7e3881605ccd361cee172
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-14 10:14:42 +0900

    [Silver I] Title: 물약 구매, Time: 1824 ms, Memory: 115248 KB -BaekjoonHub

--- 백준/Silver/24954. 물약 구매/물약 구매.py
+++ 백준/Silver/24954. 물약 구매/물약 구매.py
@@ -1,44 +1,53 @@
 N = int(input())
-costs = [0] + list(map(int, input().split()))
-# discounts[i]: i번 물약을 사면 할인(j번을 d만큼)
-discounts = [[] for _ in range(N + 1)]
+lst = list(map(int, input().split()))
 
-for n in range(1, N + 1):
+# n번째 물약을 샀을 때, [[a번째 물약을 b만큼 할인], ...]
+discounts = [[] for _ in range(N)]
+for n in range(N):
     P = int(input())
     for _ in range(P):
-        discounts[n].append(list(map(int, input().split())))
+        a, b = map(int, input().split())
+        discounts[n].append((a - 1, b))
 
+# 지금까지 방문 여부
+used = [0] * N
+# 지금까지 할인받은 총량
+total_discounts = [0] * N
+result = N * max(lst) + 1
 
-result = 21e8
-used = [0] * (N + 1)
-total_discounts = [0] * (N + 1)
 def dfs(level):
     global result
 
     if level == N:
         total = 0
 
-        for n in range(1, N + 1):
-            total += max(1, costs[n] - total_discounts[n])
-
+        for n in range(N):
+            total += max(1, lst[n] - total_discounts[n])
+        
         result = min(result, total)
         return
-
-    for n in range(1, N + 1):
+    
+    for n in range(N):
         if used[n]:
             continue
 
         used[n] = 1
         for a, b in discounts[n]:
-            if not used[a]:
-                total_discounts[a] += b
+            # 지금 물약을 사기 이전의 물약은 할인받을 수 없음
+            if used[a]:
+                continue
+
+            total_discounts[a] += b
 
         dfs(level + 1)
 
         used[n] = 0
         for a, b in discounts[n]:
-            if not used[a]:
-                total_discounts[a] -= b
+            # 지금 물약을 사기 이전의 물약은 할인받을 수 없음
+            if used[a]:
+                continue
+
+            total_discounts[a] -= b
 
 dfs(0)
 
