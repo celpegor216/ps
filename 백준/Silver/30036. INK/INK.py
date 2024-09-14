b2551d0ea3346f56c47ac2059be6d2633f6403b8
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-14 16:47:31 +0900

    [Silver I] Title: INK, Time: 120 ms, Memory: 114752 KB -BaekjoonHub

--- 백준/Silver/30036. INK/INK.py
+++ 백준/Silver/30036. INK/INK.py
@@ -2,9 +2,12 @@ from collections import deque
 
 
 I, N, K = map(int, input().split())
-inks = input()
+colors = input()
 lst = [list(input()) for _ in range(N)]
 cmds = input()
+
+directions = ((-1, 0), (1, 0), (0, -1), (0, 1))
+char_to_dir_idx = {'U': 0, 'D': 1, 'L': 2, 'R': 3}
 
 def find():
     for i in range(N):
@@ -13,44 +16,43 @@ def find():
                 lst[i][j] = '.'
                 return i, j
 
-cy, cx = find()
+y, x = find()
 ink_amount = ink_color = 0
-
-directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
-char_to_dir_idx = {'U': 3, 'D': 1, 'L': 2, 'R': 0}
 
 for cmd in cmds:
     if cmd in 'UDLR':
         dy, dx = directions[char_to_dir_idx[cmd]]
-        ny, nx = cy + dy, cx + dx
+        ny, nx = y + dy, x + dx
         if 0 <= ny < N and 0 <= nx < N and lst[ny][nx] == '.':
-            cy, cx = ny, nx
+            y, x = ny, nx
     elif cmd == 'j':
         ink_amount += 1
     elif cmd == 'J':
         if ink_amount:
             q = deque()
-            q.append((cy, cx))
+            q.append((y, x))
 
             used = [[0] * N for _ in range(N)]
+            used[y][x] = 1
 
             for _ in range(ink_amount):
                 for _ in range(len(q)):
-                    y, x = q.popleft()
+                    cy, cx = q.popleft()
 
                     for dy, dx in directions:
-                        ny, nx = y + dy, x + dx
+                        ny, nx = cy + dy, cx + dx
                         if 0 <= ny < N and 0 <= nx < N and not used[ny][nx]:
                             used[ny][nx] = 1
                             q.append((ny, nx))
 
                             if lst[ny][nx] != '.':
-                                lst[ny][nx] = inks[ink_color]
+                                lst[ny][nx] = colors[ink_color]
 
-        ink_amount = 0
+        
         ink_color = (ink_color + 1) % I
+        ink_amount = 0
 
 
-lst[cy][cx] = '@'
+lst[y][x] = '@'
 for line in lst:
     print(''.join(line))
