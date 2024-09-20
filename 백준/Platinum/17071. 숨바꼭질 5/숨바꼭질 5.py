f7b771e590b10bcc69367f49d3975a78ac3f8aba
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-20 19:01:54 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 196 ms, Memory: 129920 KB -BaekjoonHub

--- 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.py
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.py
@@ -1,39 +1,51 @@
-# 해답: https://ddingmin00.tistory.com/entry/%EB%B0%B1%EC%A4%80%ED%8C%8C%EC%9D%B4%EC%8D%AC-17071%EB%B2%88-%EC%88%A8%EB%B0%94%EA%BC%AD%EC%A7%88-5
-
-
 from collections import deque
 
 
 N, K = map(int, input().split())
-MAX = 500000
 
-# used[n]: n에 짝수/홀수 시간에 방문했을 때 최소 이동 횟수
-used = [[-1] * 2 for _ in range(MAX + 1)]
+MAX = 500000
+# used[0][i]: i에 짝수번째 횟수로 도착했을 때의 최소 횟수
+# used[1][i]: i에 홀수번째 횟수로 도착했을 때의 최소 횟수
+used = [[MAX + 1] * (MAX + 1) for _ in range(2)]
 
 q = deque()
-q.append((N, 0))
-used[N][0] = 0
+q.append(N)
 
-while q:
-    now, cnt = q.popleft()
-
-    for nxt in (now + 1, now - 1, now * 2):
-        if 0 <= nxt <= MAX and used[nxt][(cnt + 1) % 2] == -1:
-            used[nxt][(cnt + 1) % 2] = cnt + 1
-            q.append((nxt, cnt + 1))
+used[0][N] = 0
 
 result = -1
-for i in range(MAX + 1):
-    K += i
-    
-    if K > MAX:
+k = 0
+while K <= MAX:
+    if used[k % 2][K] != MAX + 1:
+        result = k
         break
 
-    # 예를 들어 K 위치에 수빈이가 4초에 도달했고,
-    # 해당 위치에 동생이 4초보다 크거나 같으면서 짝수인 시간에 도착했다면
-    # +1, -1 하면서 기다릴 수 있음
-    if used[K][i % 2] <= i:
-        result = i
+    if K - 1 >= 0 and used[(k + 1) % 2][K - 1] != MAX + 1:
+        result = k
         break
+
+    if K + 1 <= MAX and used[(k + 1) % 2][K + 1] != MAX + 1:
+        result = K
+        break
+
+    if K > 0 and not K % 2 and used[(k + 1) % 2][K // 2] != MAX + 1:
+        result = k
+        break
+
+    k += 1
+    K += k
+    for _ in range(len(q)):
+        now = q.popleft()
+
+        for nxt in (now + 1, now - 1, now * 2):
+            if not (0 <= nxt <= MAX):
+                continue
+
+            if used[k % 2][nxt] <= k:
+                continue
+
+            used[k % 2][nxt] = k
+            q.append(nxt)
+
 
 print(result)
