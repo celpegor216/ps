88914d15845c0762b2e571a83662b7020eaac36c
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-23 16:56:57 +0900

    [Gold I] Title: 열쇠, Time: 228 ms, Memory: 114076 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.py
+++ 백준/Gold/9328. 열쇠/열쇠.py
@@ -1,38 +1,53 @@
-# 메모리 초과
-# 해답: https://ji-gwang.tistory.com/449
+TC = int(input())
 
-
-from collections import deque
-
-T = int(input())
-
-for _ in range(T):
+for _ in range(TC):
     N, M = map(int, input().split())
-    lst = ['.' * (M + 2)] + ['.' + input() + '.' for _ in range(N)] + ['.' * (M + 2)]
+    lst = [['*'] * (M + 4) for _ in range(N + 4)]
+    for i in range(1, N + 3):
+        lst[i][1] = '.'
+        lst[i][-2] = '.'
+    for j in range(1, M + 3):
+        lst[1][j] = '.'
+        lst[-2][j] = '.'
+
+    for i in range(2, N + 2):
+        tmp = input()
+        for j in range(2, M + 2):
+            lst[i][j] = tmp[j - 2]
+
     keys = input()
+    if keys == '0':
+        keys = ''
 
-    q = deque()
-    q.append((0, 0))
-    used = [[0] * (M + 2) for _ in range(N + 2)]
-    used[0][0] = 1
-
-    results = set()
+    q = [(1, 1)]
+    used = [[0] * (M + 4) for _ in range(N + 4)]
+    used[1][1] = 1
 
     while q:
-        y, x = q.popleft()
+        nq = []
 
-        for dy, dx in ((0, 1), (1, 0), (0, -1), (-1, 0)):
-            ny, nx = y + dy, x + dx
-            if 0 <= ny < N + 2 and 0 <= nx < M + 2 and not used[ny][nx] and lst[ny][nx] != '*':
+        for y, x in q:
+            for dy, dx in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+                ny, nx = y + dy, x + dx
+                if lst[ny][nx] == '*' or used[ny][nx]:
+                    continue
+
                 if lst[ny][nx].isupper() and lst[ny][nx].lower() not in keys:
                     continue
-                elif lst[ny][nx].islower() and lst[ny][nx] not in keys:
-                    keys += lst[ny][nx]
-                    used = [[0] * (M + 2) for _ in range(N + 2)]
-                elif lst[ny][nx] == '$':
-                    results.add((ny, nx))
-            
-                q.append((ny, nx))
-                used[ny][nx] = 1
 
-    print(len(results))
+                if lst[ny][nx].islower() and lst[ny][nx] not in keys:
+                    keys += lst[ny][nx]
+                    used = [[0] * (M + 4) for _ in range(N + 4)]
+
+                used[ny][nx] = 1
+                nq.append((ny, nx))
+
+        q = nq
+
+    result = 0
+    for i in range(2, N + 2):
+        for j in range(2, M + 2):
+            if lst[i][j] == '$' and used[i][j]:
+                result += 1
+
+    print(result)
