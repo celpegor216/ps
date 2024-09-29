24e68b91a068050fbdea39ad846683ccfcbe5ac2
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-29 17:18:26 +0900

    [Gold IV] Title: 어드벤처 게임, Time: 448 ms, Memory: 113492 KB -BaekjoonHub

---
+++ 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.py
@@ -0,0 +1,59 @@
+# 재방문 가능하게 처리했는데 왜 틀리는거지?
+# 해답: https://magentino.tistory.com/227
+
+
+# 초 기 값,,,,,,,, -1로 설정해야 하는구나,,,,,,,,,,,,,,,
+
+
+def find():
+    if lst[1][0] < 0:
+        return 'No'
+
+    q = [(lst[1][0], 1)]
+    used = [-1] * (N + 1)
+    used[1] = lst[1][0]
+
+    while q:
+        nq = []
+
+        for cost, now in q:
+            if now == N:
+                return 'Yes'
+            
+            for nxt in lst[now][1:]:
+                nxt_cost = cost
+
+                if lst[nxt][0] < 0:
+                    nxt_cost += lst[nxt][0]
+                elif lst[nxt][0] > 0:
+                    nxt_cost = max(nxt_cost, lst[nxt][0])
+                
+                if nxt_cost < 0 or used[nxt] >= nxt_cost:
+                    continue
+                
+                used[nxt] = nxt_cost
+                nq.append((nxt_cost, nxt))
+    
+        q = nq
+
+
+    return 'No'
+
+
+
+while 1:
+    N = int(input())
+
+    if not N:
+        break
+
+    lst = [[]]
+    for _ in range(N):
+        room_type, *tmp = input().split()
+        tmp = list(map(int, tmp))
+        tmp.pop()
+        if room_type == 'T':
+            tmp[0] *= -1
+        lst.append(tmp)
+
+    print(find())
