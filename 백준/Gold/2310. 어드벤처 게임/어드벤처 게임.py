805f549eeacb870d11588671feb35c566134cf8e
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-09-29 17:19:57 +0900

    [Gold IV] Title: 어드벤처 게임, Time: 332 ms, Memory: 130520 KB -BaekjoonHub

--- 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.py
+++ 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.py
@@ -5,37 +5,43 @@
 # 초 기 값,,,,,,,, -1로 설정해야 하는구나,,,,,,,,,,,,,,,
 
 
+import heapq
+
+
 def find():
     if lst[1][0] < 0:
         return 'No'
 
-    q = [(lst[1][0], 1)]
+    q = []
+    heapq.heappush(q, (-lst[1][0], 1))
     used = [-1] * (N + 1)
     used[1] = lst[1][0]
 
     while q:
-        nq = []
+        cost, now = heapq.heappop(q)
 
-        for cost, now in q:
-            if now == N:
-                return 'Yes'
+        cost *= -1
+
+        if used[now] > cost:
+            continue
+
+        if now == N:
+            return 'Yes'
+        
+        for nxt in lst[now][1:]:
+            nxt_cost = cost
+
+            if lst[nxt][0] < 0:
+                nxt_cost += lst[nxt][0]
+            elif lst[nxt][0] > 0:
+                nxt_cost = max(nxt_cost, lst[nxt][0])
             
-            for nxt in lst[now][1:]:
-                nxt_cost = cost
-
-                if lst[nxt][0] < 0:
-                    nxt_cost += lst[nxt][0]
-                elif lst[nxt][0] > 0:
-                    nxt_cost = max(nxt_cost, lst[nxt][0])
-                
-                if nxt_cost < 0 or used[nxt] >= nxt_cost:
-                    continue
-                
-                used[nxt] = nxt_cost
-                nq.append((nxt_cost, nxt))
+            if nxt_cost < 0 or used[nxt] >= nxt_cost:
+                continue
+            
+            used[nxt] = nxt_cost
+            heapq.heappush(q, (-nxt_cost, nxt))
     
-        q = nq
-
 
     return 'No'
 
