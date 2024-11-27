29f217aca3a27311f6f88c8b8737869e4824b692
조예지 <97578535+celpegor216@users.noreply.github.com>
2024-11-27 17:41:36 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 3044 ms, Memory: 127656 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,37 +1,37 @@
-import sys, heapq
-input = sys.stdin.readline
+import heapq
 
-def find(a):
-    if group[a] != a:
-        group[a] = find(group[a])
-    return group[a]
-
-def union(a, b):
-    group_a, group_b = find(a), find(b)
-
-    if group_a != group_b:
-        if group_a > group_b:
-            group_a, group_b = group_b, group_a
-        group[group_b] = group_a
-        return 1
-    return 0
 
 V, E = map(int, input().split())
-group = [v for v in range(V + 1)]
 q = []
 
 for _ in range(E):
     a, b, c = map(int, input().split())
     heapq.heappush(q, (c, a, b))
 
+group = [x for x in range(V + 1)]
+
+
+def find(a):
+    while group[a] != a:
+        a = group[a]
+    return a
+
+
+def union(a, b):
+    group_a, group_b = find(a), find(b)
+
+    if group_a != group_b:
+        if group_a < group_b:
+            group[group_b] = group_a
+        else:
+            group[group_a] = group_b
+        return 1
+
+
 result = 0
-cnt = V - 1
-
-while cnt:
+while q:
     c, a, b = heapq.heappop(q)
-
     if union(a, b):
         result += c
-        cnt -= 1
 
 print(result)
